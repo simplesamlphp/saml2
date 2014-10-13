45d58fa6ac32c32e7b2fb8d6a32d34d71f70b93d
DRvanR <dvrenterghem@ibuildings.nl>
2014-10-13 15:57:30 +0200

    Implemented the SubjectConfirmation NotOnOrAfter check

--- src/SAML2/Assertion/Validation/ConstraintValidator/SubjectConfirmationNotOnOrAfter.php
+++ src/SAML2/Assertion/Validation/ConstraintValidator/SubjectConfirmationNotOnOrAfter.php
@@ -7,9 +7,9 @@ class SAML2_Assertion_Validation_ConstraintValidator_SubjectConfirmationNotOnOrA
         SAML2_XML_saml_SubjectConfirmation $subjectConfirmation,
         SAML2_Assertion_Validation_Result $result
     ) {
-        $notBefore = $subjectConfirmation->SubjectConfirmationData->NotBefore;
-        if ($notBefore && $notBefore > time() + 60) {
-            $result->addError('NotBefore in SubjectConfirmationData is in the future');
+        $notOnOrAfter = $subjectConfirmation->SubjectConfirmationData->NotOnOrAfter;
+        if ($notOnOrAfter && $notOnOrAfter <= time() + 60) {
+            $result->addError('NotOnOrAfter in SubjectConfirmationData is in the past');
         }
     }
 }
