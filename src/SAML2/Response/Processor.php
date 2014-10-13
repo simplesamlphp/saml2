2c8d415d6b8523826ab669298142283d1f200f49
DRvanR <dvrenterghem@ibuildings.nl>
2014-10-13 16:02:21 +0200

    Adding Assertion Processing to the Response Processor

--- src/SAML2/Response/Processor.php
+++ src/SAML2/Response/Processor.php
@@ -18,32 +18,54 @@ class SAML2_Response_Processor
     private $signatureValidator;
 
     /**
-     * @param \Psr\Log\LoggerInterface  $logger
-     * @param SAML2_Signature_Validator $signatureValidator
-     * @param string                    $expectedDestination
+     * @var SAML2_Assertion_Processor
+     */
+    private $assertionProcessor;
+
+    /**
+     * @var SAML2_Configuration_Destination
+     */
+    private $currentDestination;
+
+    /**
+     * @param \Psr\Log\LoggerInterface        $logger
+     * @param SAML2_Signature_Validator       $signatureValidator
+     * @param SAML2_Configuration_Destination $currentDestination
      */
     public function __construct(
         \Psr\Log\LoggerInterface $logger,
         SAML2_Signature_Validator $signatureValidator,
-        $expectedDestination
+        SAML2_Configuration_Destination $currentDestination
     ) {
         $this->logger = $logger;
         $this->signatureValidator = $signatureValidator;
-        $this->preconditionValidator = new SAML2_Response_Validation_PreconditionValidator($expectedDestination);
+        $this->currentDestination = $currentDestination;
+
+        $this->preconditionValidator = new SAML2_Response_Validation_PreconditionValidator($currentDestination);
     }
 
     /**
      * @param SAML2_Configuration_ServiceProvider  $serviceProviderConfiguration
      * @param SAML2_Configuration_IdentityProvider $identityProviderConfiguration
      * @param SAML2_Response                       $response
+     *
+     * @return SAML2_Assertion[] Collection (SAML2_Utilities_ArrayCollection) of SAML2_Assertion objects
      */
     public function process(
         SAML2_Configuration_ServiceProvider $serviceProviderConfiguration,
         SAML2_Configuration_IdentityProvider $identityProviderConfiguration,
         SAML2_Response $response
     ) {
+        $this->assertionProcessor = SAML2_Assertion_ProcessorBuilder::build(
+            $this->logger,
+            $this->currentDestination,
+            $identityProviderConfiguration,
+            $serviceProviderConfiguration
+        );
+
         $this->enforcePreconditions($response);
         $this->verifySignature($response, $identityProviderConfiguration);
+        return $this->processAssertions($response);
     }
 
     /**
@@ -71,5 +93,15 @@ class SAML2_Response_Processor
         if (!$this->signatureValidator->hasValidSignature($response, $identityProviderConfiguration)) {
             throw new SAML2_Response_Exception_InvalidResponseException();
         }
+    }
+
+    private function processAssertions(SAML2_Response $response)
+    {
+        $assertions = $response->getAssertions();
+        if (empty($assertions)) {
+            throw new SAML2_Response_Exception_NoAssertionsFoundException('No assertions found in response from IdP.');
+        }
+
+        return $this->assertionProcessor->processAssertions($assertions);
     }
 }
