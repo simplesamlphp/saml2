dca224dc48b72b8327397ed2de9ef35b3108f518
DRvanR <dvrenterghem@ibuildings.nl>
2014-10-16 11:02:34 +0200

    Removing unneeded comment

--- src/SAML2/Response/Exception/InvalidResponseException.php
+++ src/SAML2/Response/Exception/InvalidResponseException.php
@@ -1,8 +1,5 @@
 <?php
 
-/**
- * Marker exception to ease the integration
- */
 class SAML2_Response_Exception_InvalidResponseException extends \RuntimeException implements SAML2_Exception_Throwable
 {
 }
