30234d7245470ec50338d988abf5d51ad84851dc
Frank Tamás <sitya@niif.hu>
2015-07-27 13:10:29 +0200

    Reduce NPath Complexity

--- src/SAML2/XML/ecp/Request.php
+++ src/SAML2/XML/ecp/Request.php
@@ -6,122 +6,140 @@
  * @package simpleSAMLphp
  * @version $Id$
  */
-class SAML2_XML_ecp_Request {
+class SAML2_XML_ecp_Request
+{
 
-	/**
-	 * Name of the service provider.
-	 *
-	 * @var string|NULL
-	 */
-	public $ProviderName;
-
-
-	/**
-	 * Whether this is a passive request.
-	 *
-	 * @var boolean|NULL
-	 */
-	public $IsPassive;
+    /**
+     * Name of the service provider.
+     *
+     * @var string|NULL
+     */
+    public $ProviderName;
 
 
-	/**
-	 * The issuer of this message.
-	 *
-	 * @var string
-	 */
-	public $Issuer;
+    /**
+     * Whether this is a passive request.
+     *
+     * @var boolean|NULL
+     */
+    public $IsPassive;
 
 
-	/**
-	 * The list of acceptable IdPs.
-	 *
-	 * @var SAML2_XML_samlp_IDPList|NULL
-	 */
-	public $IDPList;
+    /**
+     * The issuer of this message.
+     *
+     * @var string
+     */
+    public $Issuer;
 
 
-	/**
-	 * Create a ECP Request element.
-	 *
-	 * @param DOMElement|NULL $xml  The XML element we should load.
-	 */
-	public function __construct(DOMElement $xml = NULL) {
-
-		if ($xml === NULL) {
-			return;
-		}
-
-		if (!$xml->hasAttributeNS(SAML2_Const::NS_SOAP, 'mustUnderstand')) {
-			throw new Exception('Missing soap-env:mustUnderstand attribute in <ecp:Request>.');
-		} elseif ($xml->getAttributeNS(SAML2_Const::NS_SOAP, 'mustUnderstand') !== '1') {
-			throw new Exception('Invalid value of soap-env:mustUnderstand attribute in <ecp:Request>.');
-		}
-
-		if (!$xml->hasAttributeNS(SAML2_Const::NS_SOAP, 'actor')) {
-			throw new Exception('Missing soap-env:mustUnderstand attribute in <ecp:Request>.');
-		} elseif ($xml->getAttributeNS(SAML2_Const::NS_SOAP, 'actor') !== 'http://schemas.xmlsoap.org/soap/actor/next') {
-			throw new Exception('Invalid value of soap-env:actor attribute in <ecp:Request>.');
-		}
-
-		if ($xml->hasAttribute('ProviderName')) {
-			$this->ProviderName = $xml->getAttribute('ProviderName');
-		}
-
-		$this->IsPassive = SAML2_Utils::parseBoolean($xml, 'IsPassive', NULL);
-
-		$issuer = SAML2_Utils::xpQuery($xml, './saml_assertion:Issuer');
-		if (empty($issuer)) {
-			throw new Exception('Missing <saml:Issuer> in <ecp:Request>.');
-		} elseif (count($issuer) > 1) {
-			throw new Exception('More than one <saml:Issuer> in <ecp:Request>.');
-		}
-		$this->Issuer = trim($issuer[0]->textContent);
-
-		$idpList = SAML2_Utils::xpQuery($xml, './saml_protocol:IDPList');
-		if (count($idpList) === 1) {
-			$this->IDPList = new SAML2_XML_samlp_IDPList($idpList[0]);
-		} elseif (count($idpList) > 1) {
-			throw new Exception('More than one <samlp:IDPList> in ECP Request.');
-		}
-	}
+    /**
+     * The list of acceptable IdPs.
+     *
+     * @var SAML2_XML_samlp_IDPList|NULL
+     */
+    public $IDPList;
 
 
-	/**
-	 * Convert this ECP Request to XML.
-	 *
-	 * @param DOMElement $parent  The element we should append this element to.
-	 */
-	public function toXML(DOMElement $parent) {
-		assert('is_string($this->ProviderName) || is_null($this->ProviderName)');
-		assert('is_bool($this->IsPassive) || is_null($this->IsPassive)');
-		assert('is_string($this->Issuer)');
-		assert('is_null($this->IDPList) || $this->IDPList instanceof SAML2_XML_samlp_IDPList');
+    /**
+     * Create a ECP Request element.
+     *
+     * @param DOMElement|NULL $xml  The XML element we should load.
+     */
+    public function __construct(DOMElement $xml = NULL)
+    {
 
-		$doc = $parent->ownerDocument;
+        if ($xml === NULL) {
+            return;
+        }
 
-		$e = $doc->createElementNS(SAML2_Const::NS_ECP, 'ecp:Request');
-		$parent->appendChild($e);
+        if ($this->checkXML($xml) !== TRUE) {
+            throw new Exception($this->checkXML($xml));
+        }
 
-		$e->setAttributeNS(SAML2_Const::NS_SOAP, 'SOAP-ENV:mustUnderstand', '1');
-		$e->setAttributeNS(SAML2_Const::NS_SOAP, 'SOAP-ENV:actor', 'http://schemas.xmlsoap.org/soap/actor/next');
+        if ($xml->hasAttribute('ProviderName')) {
+            $this->ProviderName = $xml->getAttribute('ProviderName');
+        }
 
-		if ($this->ProviderName !== NULL) {
-			$e->setAttribute('ProviderName', $this->ProviderName);
-		}
+        $this->IsPassive = SAML2_Utils::parseBoolean($xml, 'IsPassive', NULL);
 
-		if ($this->IsPassive === TRUE) {
-			$e->setAttribute('IsPassive', 'true');
-		} elseif ($this->IsPassive === FALSE) {
-			$e->setAttribute('IsPassive', 'false');
-		}
+        $issuer = SAML2_Utils::xpQuery($xml, './saml_assertion:Issuer');
+        if (empty($issuer)) {
+            throw new Exception('Missing <saml:Issuer> in <ecp:Request>.');
+        } elseif (count($issuer) > 1) {
+            throw new Exception('More than one <saml:Issuer> in <ecp:Request>.');
+        }
+        $this->Issuer = trim($issuer[0]->textContent);
 
-		SAML2_Utils::addString($e, SAML2_Const::NS_SAML, 'saml:Issuer', $this->Issuer);
+        $idpList = SAML2_Utils::xpQuery($xml, './saml_protocol:IDPList');
+        if (count($idpList) === 1) {
+            $this->IDPList = new SAML2_XML_samlp_IDPList($idpList[0]);
+        } elseif (count($idpList) > 1) {
+            throw new Exception('More than one <samlp:IDPList> in ECP Request.');
+        }
+    }
 
-		if ($this->IDPList !== NULL) {
-			$this->IDPList->toXML($e);
-		}
+    /**
+     * Basic check of the given XML, it is not part of __construct in order to reduce NPath Complexity
+     *
+     * @param DOMElement|NULL $xml  The XML element we should check before loading.
+     */
+    public function checkXML(DOMElement $xml)
+    {
 
-		return $e;
-	}
+        if (!$xml->hasAttributeNS(SAML2_Const::NS_SOAP, 'mustUnderstand')) {
+            return 'Missing soap-env:mustUnderstand attribute in <ecp:Request>.';
+        } elseif ($xml->getAttributeNS(SAML2_Const::NS_SOAP, 'mustUnderstand') !== '1') {
+            return 'Invalid value of soap-env:mustUnderstand attribute in <ecp:Request>.';
+        }
+
+        if (!$xml->hasAttributeNS(SAML2_Const::NS_SOAP, 'actor')) {
+            return 'Missing soap-env:mustUnderstand attribute in <ecp:Request>.';
+        } elseif ($xml->getAttributeNS(SAML2_Const::NS_SOAP, 'actor') !== 'http://schemas.xmlsoap.org/soap/actor/next') {
+            return 'Invalid value of soap-env:actor attribute in <ecp:Request>.';
+        }
+
+        return TRUE;
+
+    }
+
+    /**
+     * Convert this ECP Request to XML.
+     *
+     * @param DOMElement $parent  The element we should append this element to.
+     */
+    public function toXML(DOMElement $parent)
+    {
+        assert('is_string($this->ProviderName) || is_null($this->ProviderName)');
+        assert('is_bool($this->IsPassive) || is_null($this->IsPassive)');
+        assert('is_string($this->Issuer)');
+        assert('is_null($this->IDPList) || $this->IDPList instanceof SAML2_XML_samlp_IDPList');
+
+        $doc = $parent->ownerDocument;
+
+        $e = $doc->createElementNS(SAML2_Const::NS_ECP, 'ecp:Request');
+        $parent->appendChild($e);
+
+        $e->setAttributeNS(SAML2_Const::NS_SOAP, 'SOAP-ENV:mustUnderstand', '1');
+        $e->setAttributeNS(SAML2_Const::NS_SOAP, 'SOAP-ENV:actor', 'http://schemas.xmlsoap.org/soap/actor/next');
+
+        if ($this->ProviderName !== NULL) {
+            $e->setAttribute('ProviderName', $this->ProviderName);
+        }
+
+        if ($this->IsPassive === TRUE) {
+            $e->setAttribute('IsPassive', 'true');
+        } elseif ($this->IsPassive === FALSE) {
+            $e->setAttribute('IsPassive', 'false');
+        }
+
+        SAML2_Utils::addString($e, SAML2_Const::NS_SAML, 'saml:Issuer', $this->Issuer);
+
+        if ($this->IDPList !== NULL) {
+            $this->IDPList->toXML($e);
+        }
+
+        return $e;
+    }
 
 }
