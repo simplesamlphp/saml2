17c931dae450321ef1ff34c55e680e1367247a7d
Thijs Kinkhorst <thijs@kinkhorst.com>
2017-09-06 13:06:30 +0000

    Complete coverage for LogoutResponse

---
+++ tests/SAML2/LogoutResponseTest.php
@@ -0,0 +1,72 @@
+<?php
+
+namespace SAML2;
+
+/**
+ * Class \SAML2\LogoutResponseTest
+ */
+class LogoutResponseTest extends \PHPUnit_Framework_TestCase
+{
+    public function testLogoutFailed()
+    {
+        $xml = <<<XML
+<samlp:LogoutResponse xmlns:samlp="urn:oasis:names:tc:SAML:2.0:protocol"
+                xmlns:saml="urn:oasis:names:tc:SAML:2.0:assertion"
+                ID="s2a0da3504aff978b0f8c80f6a62c713c4a2f64c5b"
+                InResponseTo="_bec424fa5103428909a30ff1e31168327f79474984"
+                Version="2.0"
+                IssueInstant="2007-12-10T11:39:48Z"
+                Destination="http://somewhere.example.org/simplesaml/saml2/sp/AssertionConsumerService.php">
+    <saml:Issuer>max.example.org</saml:Issuer>
+    <samlp:Status>
+        <samlp:StatusCode Value="urn:oasis:names:tc:SAML:2.0:status:Responder" />
+        <samlp:StatusMessage>Something is wrong...</samlp:StatusMessage>
+    </samlp:Status>
+</samlp:LogoutResponse>
+XML;
+
+        $fixtureResponseDom = DOMDocumentFactory::fromString($xml);
+        $response           = new LogoutResponse($fixtureResponseDom->firstChild);
+
+        $this->assertFalse($response->isSuccess());
+
+        $status = $response->getStatus();
+        $this->assertEquals("urn:oasis:names:tc:SAML:2.0:status:Responder", $status['Code']);
+        $this->assertNull($status['SubCode']);
+        $this->assertEquals("Something is wrong...", $status['Message']);
+
+        $this->assertEquals("_bec424fa5103428909a30ff1e31168327f79474984", $response->getInResponseTo());
+    }
+
+    /**
+     * A successful logout response
+     */
+    public function testLogoutSuccess()
+    {
+        $xml = <<<XML
+<samlp:LogoutResponse xmlns:samlp="urn:oasis:names:tc:SAML:2.0:protocol"
+                xmlns:saml="urn:oasis:names:tc:SAML:2.0:assertion"
+                ID="s2a0da3504aff978b0f8c80f6a62c713c4a2f64c5b"
+                InResponseTo="_bec424fa5103428909a30ff1e31168327f79474984"
+                Version="2.0"
+                IssueInstant="2007-12-10T11:39:48Z"
+                Destination="http://somewhere.example.org/simplesaml/saml2/sp/AssertionConsumerService.php">
+    <saml:Issuer>max.example.org</saml:Issuer>
+    <samlp:Status>
+        <samlp:StatusCode Value="urn:oasis:names:tc:SAML:2.0:status:Success" />
+    </samlp:Status>
+</samlp:LogoutResponse>
+XML;
+
+        $fixtureResponseDom = DOMDocumentFactory::fromString($xml);
+        $response           = new LogoutResponse($fixtureResponseDom->firstChild);
+
+        $this->assertTrue($response->isSuccess());
+
+        $status = $response->getStatus();
+        $this->assertEquals("urn:oasis:names:tc:SAML:2.0:status:Success", $status['Code']);
+        $this->assertNull($status['SubCode']);
+        $this->assertNull($status['Message']);
+    }
+
+}
