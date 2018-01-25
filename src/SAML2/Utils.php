be97e44eb80dbc823dd0f1d4e78d668ff1f44522
Jaime Pérez Crespo <jaime.perez@uninett.no>
2018-01-25 11:26:50 +0100

    Do not break with timestamps including fractions of seconds smaller than milliseconds.

--- src/SAML2/Utils.php
+++ src/SAML2/Utils.php
@@ -691,7 +691,7 @@ class SAML2_Utils
         $matches = array();
 
         // We use a very strict regex to parse the timestamp.
-        $regex = '/^(\\d\\d\\d\\d)-(\\d\\d)-(\\d\\d)T(\\d\\d):(\\d\\d):(\\d\\d)(?:\\.\\d{1,3})?Z$/D';
+        $regex = '/^(\\d\\d\\d\\d)-(\\d\\d)-(\\d\\d)T(\\d\\d):(\\d\\d):(\\d\\d)(?:\\.\\d{1,9})?Z$/D';
         if (preg_match($regex, $time, $matches) == 0) {
             throw new Exception(
                 'Invalid SAML2 timestamp passed to xsDateTimeToTimestamp: ' . $time
