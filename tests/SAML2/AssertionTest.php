041b752c90a11492541c57b6254b51e159bc643e
Jaime Pérez Crespo <jaime.perez@uninett.no>
2018-03-02 15:43:59 +0100

    Fix build for PHP 5.3.

--- tests/SAML2/AssertionTest.php
+++ tests/SAML2/AssertionTest.php
@@ -843,7 +843,8 @@ XML;
         $result = $assertion->validate($publicKey);
 
         $this->assertTrue($result);
-        $this->assertEquals("_1bbcf227253269d19a689c53cdd542fe2384a9538b", $assertion->getNameId()['Value']);
+        $nameId = $assertion->getNameId();
+        $this->assertEquals("_1bbcf227253269d19a689c53cdd542fe2384a9538b", $nameId['Value']);
     }
 
 
