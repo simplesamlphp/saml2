321f720f6c3ef546f805f914aedb8978e981097d
Tim van Dijen <tvdijen@gmail.com>
2019-01-22 12:55:28 +0100

    Update HTTPArtifact.php

--- src/SAML2/HTTPArtifact.php
+++ src/SAML2/HTTPArtifact.php
@@ -40,7 +40,7 @@ class HTTPArtifact extends Binding
 
         $generatedId = pack('H*', bin2hex(openssl_random_pseudo_bytes(20)));
         $issuer = $message->getIssuer();
-        if ($issuer === null || $value = $issuer->getValue() === null) {
+        if ($issuer === null || ($value = $issuer->getValue()) === null) {
             throw new \Exception('Cannot get redirect URL, no Issuer set in the message.');
         }
         $artifact = base64_encode("\x00\x04\x00\x00".sha1($value, true).$generatedId);
