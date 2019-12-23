fb4ecefb49204499eecc76595583317f76027fa3
Tim van Dijen <tvdijen@gmail.com>
2019-12-23 12:26:39 +0100

    Add AuthenticatingAuthority class

---
+++ tests/SAML2/XML/saml/AuthenticatingAuthorityTest.php
@@ -0,0 +1,49 @@
+<?php
+
+declare(strict_types=1);
+
+namespace SAML2\XML\saml;
+
+use SAML2\Constants;
+use SAML2\DOMDocumentFactory;
+use SAML2\Utils;
+
+/**
+ * Class \SAML2\XML\saml\AuthenticatingAuthorityTest
+ */
+class AuthenticatingAuthorityTest extends \PHPUnit\Framework\TestCase
+{
+    /**
+     * @return void
+     */
+    public function testMarshalling(): void
+    {
+        $authority = new AuthenticatingAuthority('https://sp.example.com/SAML2');
+        $samlNamespace = Constants::NS_SAML;
+
+        $this->assertEquals(
+            '<saml:AuthenticatingAuthority xmlns:saml="' . $samlNamespace
+                . '">https://sp.example.com/SAML2</saml:AuthenticatingAuthority>',
+            strval($authority)
+        );
+    }
+
+
+    /**
+     * @return void
+     */
+    public function testUnmarshalling(): void
+    {
+        $samlNamespace = Constants::NS_SAML;
+        $document = DOMDocumentFactory::fromString(<<<XML
+<saml:AuthenticatingAuthority xmlns:saml="{$samlNamespace}">
+    https://sp.example.com/SAML2
+</saml:AuthenticatingAuthority>
+XML
+        );
+
+        /** @var \DOMElement $document->firstChild */
+        $authority = AuthenticatingAuthority::fromXML($document->firstChild);
+        $this->assertEquals('https://sp.example.com/SAML2', $authority->getAuthority());
+    }
+}
