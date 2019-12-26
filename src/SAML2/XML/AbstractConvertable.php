6508e6ab2c43e72e77382e4f6924a19b24a304e3
Tim van Dijen <tim.dijen@minbzk.nl>
2019-12-26 14:11:43 +0100

    Add AbstractConvertable class (#194)

---
+++ src/SAML2/XML/AbstractConvertable.php
@@ -0,0 +1,46 @@
+<?php
+
+declare(strict_types=1);
+
+namespace SAML2\XML;
+
+use DOMElement;
+
+/**
+ * Abstract class to be implemented by all the classes in this namespace
+ *
+ * @author Tim van Dijen, <tvdijen@gmail.com>
+ * @package SimpleSAMLphp
+ */
+
+abstract class AbstractConvertable
+{
+    /**
+     * Output the class as an XML-formatted string
+     *
+     * @return string
+     */
+    public function __toString(): string
+    {
+        $xml = $this->toXML();
+        return $xml->ownerDocument->saveXML($xml);
+    }
+
+
+    /**
+     * Create XML from this class
+     *
+     * @param \DOMElement|null $parent
+     * @return \DOMElement
+     */
+    abstract public function toXML(DOMElement $parent = null): DOMElement;
+
+
+    /**
+     * Create a class from XML
+     *
+     * @param \DOMElement $xml
+     * @return self
+     */
+    abstract public static function fromXML(DOMElement $xml): self;
+}
