c59722622975a2caeb5e9bb46fb1d02ae501ff77
Jaime Pérez Crespo <jaime.perez@uninett.no>
2020-01-27 17:16:42 +0100

    Fix Attribute and AttributeValue classes

--- tests/SAML2/XML/saml/AttributeValueTest.php
+++ tests/SAML2/XML/saml/AttributeValueTest.php
@@ -4,15 +4,43 @@ declare(strict_types=1);
 
 namespace SAML2\XML\saml;
 
+use PHPUnit\Framework\TestCase;
 use SAML2\Constants;
 use SAML2\DOMDocumentFactory;
-use SAML2\Utils;
 
 /**
- * Class \SAML2\XML\saml\AttributeTest
+ * Tests for AttributeValue elements.
+ *
+ * @package simplesamlphp/saml2
  */
-class AttributeValueTest extends \PHPUnit\Framework\TestCase
+final class AttributeValueTest extends TestCase
 {
+    protected $document;
+
+
+    protected function setUp(): void
+    {
+        $nssaml = Constants::NS_SAML;
+        $nsxs = Constants::NS_XS;
+        $nsxsi = Constants::NS_XSI;
+
+        $this->document = DOMDocumentFactory::fromString(<<<XML
+<saml:AttributeValue xmlns:saml="{$nssaml}" xmlns:xsi="{$nsxsi}" xmlns:xs="{$nsxs}" xsi:type="xs:string">value</saml:AttributeValue>
+XML
+        );
+    }
+
+
+    /**
+     * Test creating an AttributeValue from scratch.
+     */
+    public function testMarshalling(): void
+    {
+        $av = new AttributeValue('value');
+        $this->assertEquals('value', $av->getString());
+    }
+
+
     /**
      * Verifies that supplying an empty string as attribute value will
      * generate a tag with no content (instead of e.g. an empty tag).
@@ -20,70 +48,28 @@ class AttributeValueTest extends \PHPUnit\Framework\TestCase
      */
     public function testEmptyStringAttribute(): void
     {
-        $attribute = new Attribute(
-            'TheName',
-            'TheNameFormat',
-            'TheFriendlyName'
+        $av = new AttributeValue('');
+        $this->document->documentElement->textContent = '';
+        $this->assertEqualXMLStructure(
+            $this->document->documentElement,
+            $av->toXML()
         );
-        $attribute->addAttributeValue(new AttributeValue(""));
-
-        $document = DOMDocumentFactory::fromString('<root />');
-        $returnedStructure = $attribute->toXML($document->firstChild);
-
-        $expectedStructureDocument = new \DOMDocument();
-        $expectedStructureDocument->loadXML(<<<ATTRIBUTEVALUE
-<saml:Attribute xmlns:saml="urn:oasis:names:tc:SAML:2.0:assertion" Name="TheName"
- NameFormat="TheNameFormat" FriendlyName="TheFriendlyName">
-  <saml:AttributeValue xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance"
-    xmlns:xs="http://www.w3.org/2001/XMLSchema" xsi:type="xs:string"></saml:AttributeValue>
-</saml:Attribute>
-ATTRIBUTEVALUE
-        );
-        $expectedStructure = $expectedStructureDocument->documentElement;
-
-        $this->assertEqualXMLStructure($expectedStructure, $returnedStructure);
-        $this->assertEquals("", $attribute->getAttributeValue()[0]->getString());
+        $this->assertEquals('', $av->getString());
     }
 
 
-    /**
+    /**4
      * Verifies that we can create an AttributeValue from a DOMElement.
      * @return void
      */
-    public function testCreateAttributeFromDOMElement(): void
+    public function testUnmarshalling(): void
     {
-        $element = new \DOMDocument();
-        $element->loadXML(<<<ATTRIBUTEVALUE
-<NameID Format="urn:oasis:names:tc:SAML:1.1:nameid-format:unspecified">urn:collab:person:surftest.nl:example</NameID>
-ATTRIBUTEVALUE
+        $av = AttributeValue::fromXML($this->document->documentElement);
+        $this->assertEquals('value', $av->getString());
+        $this->assertEquals(
+            $this->document->saveXML($this->document->documentElement),
+            strval($av)
         );
-
-        $attribute = new Attribute(
-            'TheName',
-            'TheNameFormat',
-            'TheFriendlyName',
-            [
-                new AttributeValue($element->documentElement)
-            ]
-        );
-
-        $document = DOMDocumentFactory::fromString('<root />');
-        $returnedStructure = $attribute->toXML($document->firstChild);
-
-        $expectedStructureDocument = new \DOMDocument();
-        $expectedStructureDocument->loadXML(<<<ATTRIBUTEXML
-<saml:Attribute xmlns:saml="urn:oasis:names:tc:SAML:2.0:assertion"
-  Name="TheName" NameFormat="TheNameFormat" FriendlyName="TheFriendlyName">
-  <saml:AttributeValue>
-    <NameID Format="urn:oasis:names:tc:SAML:1.1:nameid-format:unspecified">urn:collab:person:surftest.nl:example</NameID>
-  </saml:AttributeValue>
-</saml:Attribute>
-ATTRIBUTEXML
-        );
-        $expectedStructure = $expectedStructureDocument->documentElement;
-
-        $this->assertEqualXMLStructure($expectedStructure, $returnedStructure);
-        $this->assertEquals("urn:collab:person:surftest.nl:example", $attribute->getAttributeValue()[0]->getString());
     }
 
 
