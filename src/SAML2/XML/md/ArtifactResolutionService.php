d4b44056338d0c68155c466bb190296b1b9577b2
Jaime Pérez Crespo <jaime.perez@uninett.no>
2020-01-28 12:24:55 +0100

    Migrate the EndpointType class to new immutable API.
    This means both EndpointType and IndexedEndpointType become abstract. Now we need specific classes for each instance of the abstract type. This commit provides those as well.

---
+++ src/SAML2/XML/md/ArtifactResolutionService.php
@@ -0,0 +1,14 @@
+<?php
+
+declare(strict_types=1);
+
+namespace SAML2\XML\md;
+
+/**
+ * A class implementing the md:ArtifactResolutionService element.
+ *
+ * @package simplesamlphp/saml2
+ */
+final class ArtifactResolutionService extends AbstractIndexedEndpointType
+{
+}
