3b2cf0d4fdb9af69223fef44308dbcc774473e27
Jaime Pérez Crespo <jaime.perez@uninett.no>
2020-01-30 15:07:33 +0100

    Migrate the old RoleDescriptor class to the new API
    Make it abstract, and test it via the UnknownRoleDescriptor class.

---
+++ src/SAML2/XML/md/AbstractRoleDescriptor.php
@@ -0,0 +1,261 @@
+<?php
+
+declare(strict_types=1);
+
+namespace SAML2\XML\md;
+
+use DOMElement;
+use InvalidArgumentException;
+use Webmozart\Assert\Assert;
+
+/**
+ * Class representing SAML 2 RoleDescriptor element.
+ *
+ * @package SimpleSAMLphp
+ */
+abstract class AbstractRoleDescriptor extends AbstractMetadataDocument
+{
+    /**
+     * List of supported protocols.
+     *
+     * @var array
+     */
+    protected $protocolSupportEnumeration = [];
+
+    /**
+     * Error URL for this role.
+     *
+     * @var string|null
+     */
+    protected $errorURL = null;
+
+    /**
+     * KeyDescriptor elements.
+     *
+     * Array of \SAML2\XML\md\KeyDescriptor elements.
+     *
+     * @var \SAML2\XML\md\KeyDescriptor[]
+     */
+    protected $KeyDescriptors = [];
+
+    /**
+     * Organization of this role.
+     *
+     * @var \SAML2\XML\md\Organization|null
+     */
+    protected $Organization = null;
+
+    /**
+     * ContactPerson elements for this role.
+     *
+     * Array of \SAML2\XML\md\ContactPerson objects.
+     *
+     * @var \SAML2\XML\md\ContactPerson[]
+     */
+    protected $ContactPersons = [];
+
+
+    /**
+     * Initialize a RoleDescriptor.
+     *
+     * @param string[] $protocolSupportEnumeration A set of URI specifying the protocols supported.
+     * @param string|null $ID The ID for this document. Defaults to null.
+     * @param int|null $validUntil Unix time of validity for this document. Defaults to null.
+     * @param string|null $cacheDuration Maximum time this document can be cached. Defaults to null.
+     * @param \SAML2\XML\md\Extensions|null $extensions An Extensions object. Defaults to null.
+     * @param string|null $errorURL An URI where to redirect users for support. Defaults to null.
+     * @param \SAML2\XML\md\KeyDescriptor[]|null $keyDescriptors An array of KeyDescriptor elements. Defaults to an empty array.
+     * @param \SAML2\XML\md\Organization|null $organization The organization running this entity. Defaults to null.
+     * @param \SAML2\XML\md\ContactPerson[]|null $contacts An array of contacts for this entity. Defaults to an empty array.
+     */
+    public function __construct(
+        array $protocolSupportEnumeration,
+        ?string $ID = null,
+        ?int $validUntil = null,
+        ?string $cacheDuration = null,
+        ?Extensions $extensions = null,
+        ?string $errorURL = null,
+        ?array $keyDescriptors = [],
+        ?Organization $organization = null,
+        ?array $contacts = []
+    ) {
+        parent::__construct($ID, $validUntil, $cacheDuration, $extensions);
+
+        $this->setProtocolSupportEnumeration($protocolSupportEnumeration);
+        $this->setErrorURL($errorURL);
+        $this->setKeyDescriptors($keyDescriptors);
+        $this->Organization = $organization;
+        $this->setContactPersons($contacts);
+    }
+
+
+    /**
+     * Collect the value of the errorURL property.
+     *
+     * @return string|null
+     */
+    public function getErrorURL()
+    {
+        return $this->errorURL;
+    }
+
+
+    /**
+     * Set the value of the errorURL property.
+     *
+     * @param string|null $errorURL
+     * @throws \InvalidArgumentException
+     */
+    protected function setErrorURL(?string $errorURL = null): void
+    {
+        if (!is_null($errorURL) && !filter_var($errorURL, FILTER_VALIDATE_URL)) {
+            throw new InvalidArgumentException('RoleDescriptor errorURL is not a valid URL.');
+        }
+        $this->errorURL = $errorURL;
+    }
+
+
+    /**
+     * Collect the value of the protocolSupportEnumeration property.
+     *
+     * @return string[]
+     */
+    public function getProtocolSupportEnumeration()
+    {
+        return $this->protocolSupportEnumeration;
+    }
+
+
+    /**
+     * Set the value of the ProtocolSupportEnumeration property.
+     *
+     * @param array $protocols
+     */
+    protected function setProtocolSupportEnumeration(array $protocols): void
+    {
+        Assert::minCount($protocols, 1, 'At least one protocol must be supported by this ' . static::class . '.');
+        Assert::allStringNotEmpty($protocols, 'Cannot specify an empty string as a supported protocol.');
+        $this->protocolSupportEnumeration = $protocols;
+    }
+
+
+    /**
+     * Collect the value of the Organization property.
+     *
+     * @return \SAML2\XML\md\Organization|null
+     */
+    public function getOrganization()
+    {
+        return $this->Organization;
+    }
+
+
+    /**
+     * Set the value of the Organization property.
+     *
+     * @param \SAML2\XML\md\Organization|null $organization
+     */
+    protected function setOrganization(?Organization $organization = null): void
+    {
+        $this->Organization = $organization;
+    }
+
+
+    /**
+     * Collect the value of the ContactPersons property.
+     *
+     * @return ContactPerson[]
+     */
+    public function getContactPersons()
+    {
+        return $this->ContactPersons;
+    }
+
+
+    /**
+     * Set the value of the ContactPerson property.
+     *
+     * @param ContactPerson[]|null $contactPersons
+     */
+    protected function setContactPersons(?array $contactPersons): void
+    {
+        if ($contactPersons === null) {
+            return;
+        }
+        Assert::allIsInstanceOf(
+            $contactPersons,
+            ContactPerson::class,
+            'All contacts must be an instance of md:ContactPerson'
+        );
+        $this->ContactPersons = $contactPersons;
+    }
+
+
+    /**
+     * Collect the value of the KeyDescriptors property.
+     *
+     * @return \SAML2\XML\md\KeyDescriptor[]
+     */
+    public function getKeyDescriptors()
+    {
+        return $this->KeyDescriptors;
+    }
+
+
+    /**
+     * Set the value of the KeyDescriptor property.
+     *
+     * @param \SAML2\XML\md\KeyDescriptor[]|null $keyDescriptor
+     */
+    protected function setKeyDescriptors(?array $keyDescriptor): void
+    {
+        if ($keyDescriptor === null) {
+            return;
+        }
+        Assert::allIsInstanceOf(
+            $keyDescriptor,
+            KeyDescriptor::class,
+            'All key descriptors must be an instance of md:KeyDescriptor'
+        );
+        $this->KeyDescriptors = $keyDescriptor;
+    }
+
+
+    /**
+     * Add this RoleDescriptor to an EntityDescriptor.
+     *
+     * @param \DOMElement $parent The EntityDescriptor we should append this endpoint to.
+     *
+     * @return \DOMElement
+     *
+     * @throws \InvalidArgumentException if assertions are false
+     */
+    public function toXML(?DOMElement $parent = null): DOMElement
+    {
+        $e = parent::toXML($parent);
+
+        $e->setAttribute('protocolSupportEnumeration', implode(' ', $this->protocolSupportEnumeration));
+
+        if ($this->errorURL !== null) {
+            $e->setAttribute('errorURL', $this->errorURL);
+        }
+
+        if ($this->Extensions !== null && !$this->Extensions->isEmptyElement()) {
+            $this->Extensions->toXML($e);
+        }
+
+        foreach ($this->KeyDescriptors as $kd) {
+            $kd->toXML($e);
+        }
+
+        if ($this->Organization !== null) {
+            $this->Organization->toXML($e);
+        }
+
+        foreach ($this->ContactPersons as $cp) {
+            $cp->toXML($e);
+        }
+
+        return $e;
+    }
+}
