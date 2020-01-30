7d04ca71daadd9db53e5d0b32d4b9f2d33344336
Tim van Dijen <tvdijen@gmail.com>
2020-01-30 15:12:31 +0100

    Pull in dependencies from 208

---
+++ src/SAML2/XML/md/ServiceDescription.php
@@ -0,0 +1,15 @@
+<?php
+
+declare(strict_types=1);
+
+namespace SAML2\XML\md;
+
+/**
+ * A localized name representing a service's description.
+ *
+ * @package simplesamlphp/saml2
+ */
+class ServiceDescription extends AbstractLocalizedName
+{
+}
+
