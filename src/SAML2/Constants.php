cee134d382ce17dd4ea0b352377ea4c2508dd00c
Jaime Pérez Crespo <jaime.perez@uninett.no>
2020-01-31 14:47:43 +0100

    Implement md:EncryptionMethod

--- src/SAML2/Constants.php
+++ src/SAML2/Constants.php
@@ -258,6 +258,11 @@ class Constants
     public const NS_HOK = 'urn:oasis:names:tc:SAML:2.0:profiles:holder-of-key:SSO:browser';
 
     /**
+     * The namespace for the XML encryption schema.
+     */
+    public const NS_XENC = 'http://www.w3.org/2001/04/xmlenc#';
+
+    /**
      * The status namespace
      */
     public const STATUS_PREFIX = 'urn:oasis:names:tc:SAML:2.0:status:';
