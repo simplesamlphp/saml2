a096d8821b681d21004fb6627e35a4432dab9608
Jaime Pérez Crespo <jaime.perez@uninett.no>
2020-01-31 17:55:56 +0100

    Add proper tests for SAML2\XML\md\KeyDescriptor

--- tests/SAML2/XML/md/KeyDescriptorTest.php
+++ tests/SAML2/XML/md/KeyDescriptorTest.php
@@ -8,6 +8,8 @@ use PHPUnit\Framework\TestCase;
 use SAML2\Constants;
 use SAML2\DOMDocumentFactory;
 use SAML2\XML\ds\AbstractDsElement;
+use SAML2\XML\ds\KeyInfo;
+use SAML2\XML\ds\KeyName;
 
 /**
  * A set of tests for the md:KeyDescriptor element
@@ -35,7 +37,115 @@ XML
     }
 
 
-    // @todo: add tests for marshalling / unmarshalling
+    /**
+     * Test creating a KeyDescriptor from scratch.
+     */
+    public function testMarshalling(): void
+    {
+        $kd = new KeyDescriptor(
+            new KeyInfo([new KeyName('IdentityProvider.com SSO Key')]),
+            'signing',
+            [new EncryptionMethod('http://www.w3.org/2001/04/xmlenc#rsa-1_5')]
+        );
+        $this->assertEquals('signing', $kd->getUse());
+        $knfo = $kd->getKeyInfo();
+        $this->assertInstanceOf(KeyInfo::class, $knfo);
+        $this->assertCount(1, $knfo->getInfo());
+        $this->assertInstanceOf(KeyName::class, $knfo->getInfo()[0]);
+        $this->assertCount(1, $kd->getEncryptionMethods());
+        $this->assertInstanceOf(EncryptionMethod::class, $kd->getEncryptionMethods()[0]);
+        $this->assertEquals('http://www.w3.org/2001/04/xmlenc#rsa-1_5', $kd->getEncryptionMethods()[0]->getAlgorithm());
+        $this->assertEquals(
+            $this->document->saveXML($this->document->documentElement),
+            strval($kd)
+        );
+    }
+
+
+    /**
+     * Test that creating a KeyDescriptor from scratch with a wrong use fails.
+     */
+    public function testMarshallingWrongUse(): void
+    {
+        $this->expectException(\InvalidArgumentException::class);
+        $this->expectExceptionMessage('The "use" attribute of a KeyDescriptor can only be "encryption" or "signing".');
+        new KeyDescriptor(
+            new KeyInfo([new KeyName('IdentityProvider.com SSO Key')]),
+            'wrong'
+        );
+    }
+
+
+    /**
+     * Test that creating a KeyDescriptor from scratch without any optional argument works.
+     */
+    public function testMarshallingWithoutOptionalParameters(): void
+    {
+        $kd = new KeyDescriptor(new KeyInfo([new KeyName('IdentityProvider.com SSO Key')]));
+        $this->assertNull($kd->getUse());
+        $this->assertIsArray($kd->getEncryptionMethods());
+        $this->assertEmpty($kd->getEncryptionMethods());
+        $this->assertEquals(
+            <<<XML
+<md:KeyDescriptor xmlns:md="urn:oasis:names:tc:SAML:2.0:metadata">
+  <ds:KeyInfo xmlns:ds="http://www.w3.org/2000/09/xmldsig#">
+    <ds:KeyName>IdentityProvider.com SSO Key</ds:KeyName>
+  </ds:KeyInfo>
+</md:KeyDescriptor>
+XML
+            ,
+            strval($kd)
+        );
+    }
+
+
+    /**
+     * Test creating a KeyDescriptor from XML.
+     */
+    public function testUnmarshalling(): void
+    {
+        $kd = KeyDescriptor::fromXML($this->document->documentElement);
+        $this->assertEquals('signing', $kd->getUse());
+        $knfo = $kd->getKeyInfo();
+        $this->assertInstanceOf(KeyInfo::class, $knfo);
+        $this->assertCount(1, $knfo->getInfo());
+        $this->assertInstanceOf(KeyName::class, $knfo->getInfo()[0]);
+        $this->assertCount(1, $kd->getEncryptionMethods());
+        $this->assertInstanceOf(EncryptionMethod::class, $kd->getEncryptionMethods()[0]);
+        $this->assertEquals('http://www.w3.org/2001/04/xmlenc#rsa-1_5', $kd->getEncryptionMethods()[0]->getAlgorithm());
+    }
+
+
+    /**
+     * Test that creating a KeyDescriptor from XML with a wrong use fails.
+     */
+    public function testUnmarshallingWithWrongUse(): void
+    {
+        $this->document->documentElement->setAttribute('use', 'wrong');
+        $this->expectException(\InvalidArgumentException::class);
+        $this->expectExceptionMessage('The "use" attribute of a KeyDescriptor can only be "encryption" or "signing".');
+        KeyDescriptor::fromXML($this->document->documentElement);
+    }
+
+
+    /**
+     * Test that creating a KeyDescriptor from XML works when no optional elements are present.
+     */
+    public function testUnmarshallingWithoutOptionalParameters(): void
+    {
+        $document = DOMDocumentFactory::fromString(<<<XML
+<md:KeyDescriptor xmlns:md="urn:oasis:names:tc:SAML:2.0:metadata">
+  <ds:KeyInfo xmlns:ds="http://www.w3.org/2000/09/xmldsig#">
+    <ds:KeyName>IdentityProvider.com SSO Key</ds:KeyName>
+  </ds:KeyInfo>
+</md:KeyDescriptor>
+XML
+        );
+        $kd = KeyDescriptor::fromXML($document->documentElement);
+        $this->assertNull($kd->getUse());
+        $this->assertIsArray($kd->getEncryptionMethods());
+        $this->assertEmpty($kd->getEncryptionMethods());
+    }
 
 
     /**
