168c0ab51381daaae98e6a015b990b404191f7aa
Jaime Pérez Crespo <jaime.perez@uninett.no>
2020-02-01 16:16:07 +0100

    Implement SingleSignOnService endpoint

---
+++ src/SAML2/XML/md/SingleSignOnService.php
@@ -0,0 +1,38 @@
+<?php
+
+declare(strict_types=1);
+
+namespace SAML2\XML\md;
+
+use Webmozart\Assert\Assert;
+
+/**
+ * Class representing an md:SingleSignOnService element.
+ *
+ * @package simplesamlphp/saml2
+ */
+final class SingleSignOnService extends AbstractEndpointType
+{
+    /**
+     * SingleSignOnService constructor.
+     *
+     * This is an endpoint with one restriction: it cannot contain a ResponseLocation.
+     *
+     * @param string $binding
+     * @param string $location
+     * @param string|null $unused
+     * @param array|null $attributes
+     */
+    public function __construct(
+        string $binding,
+        string $location,
+        ?string $unused = null,
+        ?array $attributes = null
+    ) {
+        Assert::null(
+            $unused,
+            'The \'ResponseLocation\' attribute must be omitted for md:SingleSignOnService.'
+        );
+        parent::__construct($binding, $location, null, $attributes);
+    }
+}
