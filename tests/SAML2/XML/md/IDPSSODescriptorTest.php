3f3b92dfa6e9995e86603b749480875011a49265
Jaime Pérez Crespo <jaime.perez@uninett.no>
2020-02-03 14:03:18 +0100

    Add initial, basic test for IDPSSODescriptor
    For now, it just checks serialization / unserialization

---
+++ tests/SAML2/XML/md/IDPSSODescriptorTest.php
@@ -0,0 +1,68 @@
+<?php
+
+declare(strict_types=1);
+
+namespace SAML2\XML\md;
+
+use PHPUnit\Framework\TestCase;
+use RobRichards\XMLSecLibs\XMLSecurityDSig;
+use SAML2\Constants;
+use SAML2\DOMDocumentFactory;
+
+/**
+ * Tests for IDPSSODescriptor.
+ *
+ * @package simplesamlphp/saml2
+ */
+class IDPSSODescriptorTest extends TestCase
+{
+    protected $document;
+
+
+    protected function setUp(): void
+    {
+        $mdns = Constants::NS_MD;
+        $dsns = XMLSecurityDSig::XMLDSIGNS;
+        $samlns = Constants::NS_SAML;
+        $this->document = DOMDocumentFactory::fromString(<<<XML
+<md:IDPSSODescriptor xmlns:md="{$mdns}" protocolSupportEnumeration="urn:oasis:names:tc:SAML:2.0:protocol"
+WantAuthnRequestsSigned="true">
+  <md:KeyDescriptor use="signing">
+    <ds:KeyInfo xmlns:ds="{$dsns}">
+      <ds:KeyName>IdentityProvider.com SSO Key</ds:KeyName>
+    </ds:KeyInfo>
+  </md:KeyDescriptor>
+  <md:ArtifactResolutionService Binding="urn:oasis:names:tc:SAML:2.0:bindings:SOAP" Location="https://IdentityProvider.com/SAML/Artifact" index="0" isDefault="true"/>
+  <md:SingleLogoutService Binding="urn:oasis:names:tc:SAML:2.0:bindings:SOAP" Location="https://IdentityProvider.com/SAML/SLO/SOAP"/>
+  <md:SingleLogoutService Binding="urn:oasis:names:tc:SAML:2.0:bindings:HTTP-Redirect" Location="https://IdentityProvider.com/SAML/SLO/Browser" ResponseLocation="https://IdentityProvider.com/SAML/SLO/Response"/>
+  <md:ManageNameIDService Binding="urn:oasis:names:tc:SAML:2.0:bindings:HTTP-POST" Location="https://IdentityProvider.com/SAML/SSO/Browser"/>
+  <md:NameIDFormat>urn:oasis:names:tc:SAML:1.1:nameid-format:X509SubjectName</md:NameIDFormat>
+  <md:NameIDFormat>urn:oasis:names:tc:SAML:2.0:nameid-format:persistent</md:NameIDFormat>
+  <md:NameIDFormat>urn:oasis:names:tc:SAML:2.0:nameid-format:transient</md:NameIDFormat>
+  <md:SingleSignOnService Binding="urn:oasis:names:tc:SAML:2.0:bindings:HTTP-Redirect" Location="https://IdentityProvider.com/SAML/SSO/Browser"/>
+  <md:SingleSignOnService Binding="urn:oasis:names:tc:SAML:2.0:bindings:HTTP-POST" Location="https://IdentityProvider.com/SAML/SSO/Browser"/>
+  <saml:Attribute xmlns:saml="{$samlns}" Name="urn:oid:1.3.6.1.4.1.5923.1.1.1.6" NameFormat="urn:oasis:names:tc:SAML:2.0:attrname-format:uri" FriendlyName="eduPersonPrincipalName"></saml:Attribute>
+  <saml:Attribute xmlns:saml="{$samlns}" Name="urn:oid:1.3.6.1.4.1.5923.1.1.1.1" NameFormat="urn:oasis:names:tc:SAML:2.0:attrname-format:uri" FriendlyName="eduPersonAffiliation">
+    <saml:AttributeValue>member</saml:AttributeValue>
+    <saml:AttributeValue>student</saml:AttributeValue>
+    <saml:AttributeValue>faculty</saml:AttributeValue>
+    <saml:AttributeValue>employee</saml:AttributeValue>
+    <saml:AttributeValue>staff</saml:AttributeValue>
+  </saml:Attribute>
+</md:IDPSSODescriptor>
+XML
+        );
+    }
+
+
+    /**
+     * Test serialization / unserialization.
+     */
+    public function testSerialize(): void
+    {
+        $this->assertEquals(
+            $this->document->saveXML($this->document->documentElement),
+            strval(unserialize(serialize(IDPSSODescriptor::fromXML($this->document->documentElement))))
+        );
+    }
+}
