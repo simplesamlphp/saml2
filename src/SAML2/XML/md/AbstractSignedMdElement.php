d17562af5cf664caa576ed1442c6bc9e99d77752
Jaime Pérez Crespo <jaime.perez@uninett.no>
2020-02-13 15:48:55 +0100

    Refactor SignedElementTrait to use the new class modeling ds:Signature
    Move also the remaining methods in AbstractSignedMdElement to the trait.

--- src/SAML2/XML/md/AbstractSignedMdElement.php
+++ src/SAML2/XML/md/AbstractSignedMdElement.php
@@ -4,7 +4,6 @@ declare(strict_types=1);
 
 namespace SAML2\XML\md;
 
-use RobRichards\XMLSecLibs\XMLSecurityKey;
 use SAML2\SignedElementTrait;
 use SAML2\SignedElementInterface;
 
@@ -16,60 +15,4 @@ use SAML2\SignedElementInterface;
 abstract class AbstractSignedMdElement extends AbstractMdElement implements SignedElementInterface
 {
     use SignedElementTrait;
-
-    /**
-     * List of certificates that should be included in the message.
-     *
-     * @var string[]
-     */
-    protected $certificates = [];
-
-
-    /**
-     * Retrieve the certificates that are included in the message.
-     *
-     * @return string[] An array of certificates
-     */
-    public function getCertificates(): array
-    {
-        return $this->certificates;
-    }
-
-
-    /**
-     * Set the certificates that should be included in the element.
-     * The certificates should be strings with the PEM encoded data.
-     *
-     * @param string[] $certificates An array of certificates.
-     * @return void
-     */
-    public function setCertificates(array $certificates): void
-    {
-        $this->certificates = $certificates;
-    }
-
-
-    /**
-     * Retrieve the private key we should use to sign the message.
-     *
-     * @return \RobRichards\XMLSecLibs\XMLSecurityKey|null The key, or NULL if no key is specified
-     */
-    public function getSignatureKey(): ?XMLSecurityKey
-    {
-        return $this->signatureKey;
-    }
-
-
-    /**
-     * Set the private key we should use to sign the message.
-     *
-     * If the key is null, the message will be sent unsigned.
-     *
-     * @param \RobRichards\XMLSecLibs\XMLSecurityKey|null $signatureKey
-     * @return void
-     */
-    public function setSignatureKey(XMLSecurityKey $signatureKey = null): void
-    {
-        $this->signatureKey = $signatureKey;
-    }
 }
