c7d928584ea4781004d2a723aba37016b8238b7e
Jaime Pérez Crespo <jaime.perez@uninett.no>
2020-02-13 16:01:04 +0100

    Get rid of some redundant, skipped tests

--- tests/SAML2/SignedElementHelperTest.php
+++ tests/SAML2/SignedElementHelperTest.php
@@ -6,7 +6,6 @@ namespace SAML2;
 
 use Exception;
 use PHPUnit\Framework\TestCase;
-use RobRichards\XMLSecLibs\XMLSecurityDSig;
 use RobRichards\XMLSecLibs\XMLSecurityKey;
 
 /**
@@ -171,74 +170,5 @@ XML
         $assertion2 = new Assertion($xml);
         $this->assertTrue($assertion2->validate(CertificatesMock::getPublicKeySha256()));
         $this->assertEquals([CertificatesMock::getPlainPublicKeyContents()], $assertion2->getCertificates());
-    }
-
-
-    /**
-     * Test that signatures contain the corresponding public keys.
-     * @return void
-     */
-    public function testGetValidatingCertificates(): void
-    {
-        $this->markTestSkipped();
-        $certData = XMLSecurityDSig::staticGet509XCerts(CertificatesMock::PUBLIC_KEY_PEM);
-        $certData = $certData[0];
-
-        $signedMockElementCopy = Utils::copyElement($this->signedMockElement);
-        $signedMockElementCopy->ownerDocument->appendChild($signedMockElementCopy);
-        $tmp = new SignedElementHelperMock($signedMockElementCopy);
-        $certs = $tmp->getValidatingCertificates();
-        $this->assertCount(1, $certs);
-        $this->assertEquals($certData, $certs[0]);
-
-        // Test with two certificates.
-        $tmpCert = '-----BEGIN CERTIFICATE-----
-MIICsDCCAhmgAwIBAgIJALU2mjA9ULI2MA0GCSqGSIb3DQEBBQUAMEUxCzAJBgNV
-BAYTAkFVMRMwEQYDVQQIEwpTb21lLVN0YXRlMSEwHwYDVQQKExhJbnRlcm5ldCBX
-aWRnaXRzIFB0eSBMdGQwHhcNMTAwODAzMDYzNTQ4WhcNMjAwODAyMDYzNTQ4WjBF
-MQswCQYDVQQGEwJBVTETMBEGA1UECBMKU29tZS1TdGF0ZTEhMB8GA1UEChMYSW50
-ZXJuZXQgV2lkZ2l0cyBQdHkgTHRkMIGfMA0GCSqGSIb3DQEBAQUAA4GNADCBiQKB
-gQDG6q53nl3Gn/9JE+ZiCgEB+EPcGbvzi0NrBDkKz9SKBNflxKQ+De/OAVQ9RQZO
-tEm/j0hoSCGO7maemOm1PVNtDuMchSroPs0L4szLhh6m1uMhw9RXqq34C+Cr7Wee
-ZNPQTFnQhBYqnYM03/e3SeUawiZ7rGeAMJ/8BSk0CB1GAQIDAQABo4GnMIGkMB0G
-A1UdDgQWBBRnHHPiQ/pV/xDZg3EBmU3ik64ORDB1BgNVHSMEbjBsgBRnHHPiQ/pV
-/xDZg3EBmU3ik64ORKFJpEcwRTELMAkGA1UEBhMCQVUxEzARBgNVBAgTClNvbWUt
-U3RhdGUxITAfBgNVBAoTGEludGVybmV0IFdpZGdpdHMgUHR5IEx0ZIIJALU2mjA9
-ULI2MAwGA1UdEwQFMAMBAf8wDQYJKoZIhvcNAQEFBQADgYEAScv7ee6QajoSM4c4
-+fX+eYdjHFsvtqHD0ng987viS8eGjIrRfKAMHVzzs1jSU0TxMM7WUFDf6FpjW+Do
-r+X+X2Al/n6aDn7qAxXbl0RZuB+saxn+yFR6HFKggwkR1L2pimCuD0gTr6LlrNgf
-edF1YfJgq35hcMMLY9RE/0C0bCI=
------END CERTIFICATE-----';
-        $mock = new SignedElementHelperMock();
-        $mock->setSignatureKey(CertificatesMock::getPrivateKey());
-        $mock->setCertificates([$tmpCert, CertificatesMock::PUBLIC_KEY_PEM]);
-        $this->signedMockElement = $mock->toSignedXML();
-        $tmp = new SignedElementHelperMock($this->signedMockElement);
-        $certs = $tmp->getValidatingCertificates();
-        $this->assertCount(1, $certs);
-        $this->assertEquals($certData, $certs[0]);
-    }
-
-
-    /**
-     * @return void
-     */
-    public function testGetSignatureKeyCertificates(): void
-    {
-        $this->markTestSkipped();
-        $seh = new SignedElementHelperMock();
-        $origkey = CertificatesMock::getPrivateKey();
-        $origcerts = [CertificatesMock::PUBLIC_KEY_PEM];
-
-        $seh->setSignatureKey($origkey);
-        $seh->setCertificates($origcerts);
-
-        $key = $seh->getSignatureKey();
-
-        $this->assertInstanceOf(\RobRichards\XMLSecLibs\XMLSecurityKey::class, $key);
-        $this->assertEquals($origkey, $key);
-
-        $certs = $seh->getCertificates();
-        $this->assertEquals($origcerts, $certs);
     }
 }
