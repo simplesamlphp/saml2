401f0702518a934293fab694bc4a74e1eb484121
Tim van Dijen <tvdijen@gmail.com>
2020-02-28 23:30:21 +0100

    Fix saml:Attribute to allow for arbitrary attributes

--- tests/SAML2/XML/saml/AttributeTest.php
+++ tests/SAML2/XML/saml/AttributeTest.php
@@ -25,7 +25,7 @@ final class AttributeTest extends TestCase
     {
         $samlNamespace = Constants::NS_SAML;
         $this->document = DOMDocumentFactory::fromString(<<<XML
-<saml:Attribute xmlns:saml="{$samlNamespace}" Name="TheName" NameFormat="TheNameFormat" FriendlyName="TheFriendlyName">
+<saml:Attribute xmlns:saml="{$samlNamespace}" Name="TheName" NameFormat="TheNameFormat" FriendlyName="TheFriendlyName" test:attr1="testval1" test:attr2="testval2" xmlns:test="urn:test">
   <saml:AttributeValue xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance" xmlns:xs="http://www.w3.org/2001/XMLSchema" xsi:type="xs:string">FirstValue</saml:AttributeValue>
   <saml:AttributeValue xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance" xmlns:xs="http://www.w3.org/2001/XMLSchema" xsi:type="xs:string">SecondValue</saml:AttributeValue>
 </saml:Attribute>
@@ -42,6 +42,11 @@ XML
      */
     public function testMarshalling(): void
     {
+        $attr1 = $this->document->createAttributeNS('urn:test', 'test:attr1');
+        $attr1->value = 'testval1';
+        $attr2 = $this->document->createAttributeNS('urn:test', 'test:attr2');
+        $attr2->value = 'testval2';
+
         $attribute = new Attribute(
             'TheName',
             'TheNameFormat',
@@ -49,12 +54,16 @@ XML
             [
                 new AttributeValue('FirstValue'),
                 new AttributeValue('SecondValue')
-            ]
+            ],
+            [$attr1, $attr2]
         );
 
         $this->assertEquals('TheName', $attribute->getName());
         $this->assertEquals('TheNameFormat', $attribute->getNameFormat());
         $this->assertEquals('TheFriendlyName', $attribute->getFriendlyName());
+
+        $this->assertEquals('testval1', $attribute->getAttributeNS('urn:test', 'attr1'));
+        $this->assertEquals('testval2', $attribute->getAttributeNS('urn:test', 'attr2'));
 
         $values = $attribute->getAttributeValues();
         $this->assertCount(2, $values);
@@ -84,6 +93,24 @@ XML
         $this->assertCount(2, $attribute->getAttributeValues());
         $this->assertEquals('FirstValue', $attribute->getAttributeValues()[0]->getString());
         $this->assertEquals('SecondValue', $attribute->getAttributeValues()[1]->getString());
+
+        $this->assertEquals(
+            [
+                '{urn:test}attr1' => [
+                    'qualifiedName' => 'test:attr1',
+                    'namespaceURI' => 'urn:test',
+                    'value' => 'testval1'
+                ],
+                '{urn:test}attr2' => [
+                    'qualifiedName' => 'test:attr2',
+                    'namespaceURI' => 'urn:test',
+                    'value' => 'testval2'
+                ]
+            ],
+            $attribute->getAttributesNS()
+        );
+        $this->assertEquals('testval1', $attribute->getAttributeNS('urn:test', 'attr1'));
+        $this->assertEquals('testval2', $attribute->getAttributeNS('urn:test', 'attr2'));
     }
 
 
