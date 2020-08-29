e8e2070dd264f75d5ace0783284c6973811c0cf3
Tim van Dijen <tvdijen@gmail.com>
2020-08-29 12:25:02 +0200

    Migrate namespace

--- src/SAML2/XML/md/EncryptionMethod.php
+++ src/SAML2/XML/md/EncryptionMethod.php
@@ -5,7 +5,7 @@ declare(strict_types=1);
 namespace SimpleSAML\SAML2\XML\md;
 
 use SimpleSAML\SAML2\Constants;
-use SimpleSAML\SAML2\XML\xenc\EncryptionMethod ass EncMethod;
+use SimpleSAML\SAML2\XML\xenc\EncryptionMethod as EncMethod;
 
 /**
  * A class implementing the md:EncryptionMethod element.
