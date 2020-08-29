0ed2b89c60c45892c3c014c6264c668fa09fc11e
Tim van Dijen <tvdijen@gmail.com>
2020-08-29 22:40:22 +0200

    Use XML constants from xml-common

--- src/SAML2/Constants.php
+++ src/SAML2/Constants.php
@@ -228,41 +228,6 @@ class Constants extends \SimpleSAML\XML\Constants
     public const NAMEID_X509_SUBJECT_NAME = 'urn:oasis:names:tc:SAML:1.1:nameid-format:X509SubjectName';
 
     /**
-     * The namespace for the ECP protocol.
-     */
-    public const NS_ECP = 'urn:oasis:names:tc:SAML:2.0:profiles:SSO:ecp';
-
-    /**
-     * The namespace for the SAML 2 HoK Web Browser SSO Profile.
-     */
-    public const NS_HOK = 'urn:oasis:names:tc:SAML:2.0:profiles:holder-of-key:SSO:browser';
-
-    /**
-     * The namespace for the SAML 2 metadata.
-     */
-    public const NS_MD = 'urn:oasis:names:tc:SAML:2.0:metadata';
-
-    /**
-     * The namespace for the SAML 2 assertions.
-     */
-    public const NS_SAML = 'urn:oasis:names:tc:SAML:2.0:assertion';
-
-    /**
-     * The namespace for the SAML 2 protocol.
-     */
-    public const NS_SAMLP = 'urn:oasis:names:tc:SAML:2.0:protocol';
-
-    /**
-     * The namespace for the SOAP protocol.
-     */
-    public const NS_SOAP = 'http://schemas.xmlsoap.org/soap/envelope/';
-
-    /**
-     * The namespace for the XML encryption schema.
-     */
-    public const NS_XENC = 'http://www.w3.org/2001/04/xmlenc#';
-
-    /**
      * The responding provider was unable to successfully authenticate the principal.
      *
      * Second-level status code.
