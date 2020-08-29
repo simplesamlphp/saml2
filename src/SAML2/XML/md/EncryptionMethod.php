6872db3132955f3209f1ae5adde5435c25468023
Tim van Dijen <tvdijen@gmail.com>
2020-08-29 02:08:26 +0200

    Migrate namespace

--- src/SAML2/XML/md/EncryptionMethod.php
+++ src/SAML2/XML/md/EncryptionMethod.php
@@ -5,14 +5,14 @@ declare(strict_types=1);
 namespace SAML2\XML\md;
 
 use SimpleSAML\SAML2\Constants;
-use SimpleSAML\SAML2\XML\xenc\EncryptionMethod;
+use SimpleSAML\SAML2\XML\xenc\EncryptionMethod ass EncMethod;
 
 /**
  * A class implementing the md:EncryptionMethod element.
  *
  * @package simplesamlphp/saml2
  */
-class EncryptionMethod extends EncryptionMethod
+class EncryptionMethod extends EncMethod
 {
     /** @var string */
     public const NS = Constants::NS_MD;
