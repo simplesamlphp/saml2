c4a538f8ab266e39e463f8220f8e8d1a459df0e2
Tim van Dijen <tvdijen@gmail.com>
2020-08-31 17:39:50 +0200

    Migrate mdattr:test-xml-blobs to xml-files

--- tests/SAML2/XML/mdattr/EntityAttributesTest.php
+++ tests/SAML2/XML/mdattr/EntityAttributesTest.php
@@ -21,27 +21,41 @@ use SimpleSAML\SAML2\Utils;
  */
 final class EntityAttributesTest extends TestCase
 {
+    /** @var \DOMDocument */
+    private $document;
+
+
+    /**
+     * @return void
+     */
+    public function setUp(): void
+    {
+        $this->document = DOMDocumentFactory::fromFile(
+            dirname(dirname(dirname(dirname(__FILE__)))) . '/resources/xml/mdattr_EntityAttributes.xml'
+        );
+    }
+
+
     /**
      * @return void
      */
     public function testMarshalling(): void
     {
         $attribute1 = new Attribute(
-            'urn:simplesamlphp:v1:simplesamlphp',
+            'attrib1',
             Constants::NAMEFORMAT_URI,
             null,
-            [
-                new AttributeValue('FirstValue'),
-                new AttributeValue('SecondValue'),
-            ]
+            []
         );
 
         $attribute2 = new Attribute(
             'foo',
-            'urn:simplesamlphp:v1',
+            'urn:simplesamlphp:v1:simplesamlphp',
             null,
             [
-                new AttributeValue('bar')
+                new AttributeValue('is'),
+                new AttributeValue('really'),
+                new AttributeValue('cool')
             ]
         );
 
@@ -71,59 +85,36 @@ final class EntityAttributesTest extends TestCase
      */
     public function testUnmarshalling(): void
     {
-        $document = DOMDocumentFactory::fromString(<<<XML
-<mdattr:EntityAttributes xmlns:mdattr="urn:oasis:names:tc:SAML:metadata:attribute">
-    <saml:Assertion xmlns:saml="urn:oasis:names:tc:SAML:2.0:assertion" IssueInstant="1984-08-26T10:01:30.000Z" Version="2.0"/>
-    <saml:Attribute xmlns:saml="urn:oasis:names:tc:SAML:2.0:assertion" Name="attrib1"/>
-    <saml:Assertion xmlns:saml="urn:oasis:names:tc:SAML:2.0:assertion" IssueInstant="1984-08-26T10:01:30.000Z" Version="2.0"/>
-    <saml:Attribute xmlns:saml="urn:oasis:names:tc:SAML:2.0:assertion" Name="attrib2"/>
-    <saml:Attribute xmlns:saml="urn:oasis:names:tc:SAML:2.0:assertion" Name="attrib3"/>
-</mdattr:EntityAttributes>
-XML
-        );
-
-        $entityAttributes = EntityAttributes::fromXML($document->firstChild);
-        $this->assertCount(5, $entityAttributes->getChildren());
+        $entityAttributes = EntityAttributes::fromXML($this->document->firstChild);
+        $this->assertCount(4, $entityAttributes->getChildren());
 
         $this->assertInstanceOf(Chunk::class, $entityAttributes->getChildren()[0]);
         $this->assertInstanceOf(Attribute::class, $entityAttributes->getChildren()[1]);
         $this->assertInstanceOf(Chunk::class, $entityAttributes->getChildren()[2]);
         $this->assertInstanceOf(Attribute::class, $entityAttributes->getChildren()[3]);
-        $this->assertInstanceOf(Attribute::class, $entityAttributes->getChildren()[4]);
 
         $this->assertEquals('Assertion', $entityAttributes->getChildren()[0]->getLocalName());
         $this->assertEquals('1984-08-26T10:01:30.000Z', $entityAttributes->getChildren()[0]->getXML()->getAttribute('IssueInstant'));
-        $this->assertEquals('attrib2', $entityAttributes->getChildren()[3]->getName());
+        $this->assertEquals('attrib1', $entityAttributes->getChildren()[1]->getName());
+        $this->assertEquals('urn:oasis:names:tc:SAML:2.0:attrname-format:uri', $entityAttributes->getChildren()[1]->getNameFormat());
+        $this->assertCount(0, $entityAttributes->getChildren()[1]->getAttributeValues());
+        $this->assertEquals('Assertion', $entityAttributes->getChildren()[2]->getLocalName());
+        $this->assertEquals('1984-08-26T10:01:30.000Z', $entityAttributes->getChildren()[2]->getXML()->getAttribute('IssueInstant'));
+        $this->assertEquals('urn:simplesamlphp:v1:simplesamlphp', $entityAttributes->getChildren()[3]->getName());
+        $this->assertEquals('urn:oasis:names:tc:SAML:2.0:attrname-format:uri', $entityAttributes->getChildren()[3]->getNameFormat());
+        $this->assertCount(3, $entityAttributes->getChildren()[3]->getAttributeValues());
     }
 
 
     /**
-     * @return void
+     * Test serialization and unserialization of EntityAttributes elements.
      */
-    public function testUnmarshallingAttributes(): void
+    public function testSerialization(): void
     {
-        $document = DOMDocumentFactory::fromString(<<<XML
-<mdattr:EntityAttributes xmlns:mdattr="urn:oasis:names:tc:SAML:metadata:attribute">
-  <saml:Attribute xmlns:saml="urn:oasis:names:tc:SAML:2.0:assertion" Name="urn:simplesamlphp:v1:simplesamlphp" NameFormat="urn:oasis:names:tc:SAML:2.0:attrname-format:uri">
-    <saml:AttributeValue xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance" xmlns:xs="http://www.w3.org/2001/XMLSchema" xsi:type="xs:string">is</saml:AttributeValue>
-    <saml:AttributeValue xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance" xmlns:xs="http://www.w3.org/2001/XMLSchema" xsi:type="xs:string">really</saml:AttributeValue>
-    <saml:AttributeValue xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance" xmlns:xs="http://www.w3.org/2001/XMLSchema" xsi:type="xs:string">cool</saml:AttributeValue>
-  </saml:Attribute>
-  <saml:Attribute xmlns:saml="urn:oasis:names:tc:SAML:2.0:assertion" Name="foo" NameFormat="urn:simplesamlphp:v1">
-    <saml:AttributeValue xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance" xmlns:xs="http://www.w3.org/2001/XMLSchema" xsi:type="xs:string">bar</saml:AttributeValue>
-  </saml:Attribute>
-</mdattr:EntityAttributes>
-XML
+        $ea = EntityAttributes::fromXML($this->document->documentElement);
+        $this->assertEquals(
+            $this->document->saveXML($this->document->documentElement),
+            strval(unserialize(serialize($ea)))
         );
-
-        $entityAttributes = EntityAttributes::fromXML($document->firstChild);
-        $this->assertCount(2, $entityAttributes->getChildren());
-
-        $this->assertEquals('urn:simplesamlphp:v1:simplesamlphp', $entityAttributes->getChildren()[0]->getName());
-        $this->assertEquals('urn:oasis:names:tc:SAML:2.0:attrname-format:uri', $entityAttributes->getChildren()[0]->getNameFormat());
-        $this->assertCount(3, $entityAttributes->getChildren()[0]->getAttributeValues());
-        $this->assertEquals('foo', $entityAttributes->getChildren()[1]->getName());
-        $this->assertEquals('urn:simplesamlphp:v1', $entityAttributes->getChildren()[1]->getNameFormat());
-        $this->assertCount(1, $entityAttributes->getChildren()[1]->getAttributeValues());
     }
 }
