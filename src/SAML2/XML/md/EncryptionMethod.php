416a16dddea9cfd641669b6e0c52bffa908433d4
Tim van Dijen <tvdijen@gmail.com>
2020-09-01 23:49:45 +0200

    Fix

--- src/SAML2/XML/md/EncryptionMethod.php
+++ src/SAML2/XML/md/EncryptionMethod.php
@@ -5,18 +5,36 @@ declare(strict_types=1);
 namespace SimpleSAML\SAML2\XML\md;
 
 use SimpleSAML\SAML2\Constants;
-use SimpleSAML\SAML2\XML\xenc\EncryptionMethod as EncMethod;
+use SimpleSAML\SAML2\XML\xenc\AbstractEncryptionMethod;
 
 /**
  * A class implementing the md:EncryptionMethod element.
  *
  * @package simplesamlphp/saml2
  */
-class EncryptionMethod extends EncMethod
+class EncryptionMethod extends AbstractEncryptionMethod
 {
     /** @var string */
     public const NS = Constants::NS_MD;
 
     /** @var string */
     public const NS_PREFIX = 'md';
+
+
+    /*
+     * EncryptionMethod constructor.
+     *
+     * @param string $algorithm
+     * @param int|null $keySize
+     * @param string|null $oaepParams
+     * @param \SimpleSAML\XML\Chunk[] $children
+     */
+    public function __construct(
+        string $algorithm,
+        ?int $keySize = null,
+        ?string $oaepParams = null,
+        array $children = []
+    ) {
+        parent::__construct($algorithm, $keySize, $oaepParams, $children);
+    }
 }
