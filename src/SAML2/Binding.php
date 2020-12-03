91a8464cba966daaa02ce46f20d95103a844a056
Pavel Břoušek <melanger@users.noreply.github.com>
2020-12-03 19:14:09 +0100

    Support ECP from Microsoft (#254)
    * Support ECP from Microsoft
    
    Add content type application/soap+xml for SOAP requests (ECP).

--- src/SAML2/Binding.php
+++ src/SAML2/Binding.php
@@ -87,7 +87,7 @@ abstract class Binding
                     return new HTTPPost();
                 } elseif (array_key_exists('SAMLart', $_POST)) {
                     return new HTTPArtifact();
-                } elseif ($contentType === 'text/xml') {
+                } elseif ($contentType === 'text/xml' || $contentType === 'application/soap+xml') {
                     return new SOAP();
                 }
                 break;
