071ed8a9f0937fc088c9f0b24f8697a3b59adfee
Tim van Dijen <tvdijen@gmail.com>
2021-03-14 16:12:03 +0100

    Add md:OrganizationUrl class

---
+++ src/SAML2/XML/md/OrganizationURL.php
@@ -0,0 +1,14 @@
+<?php
+
+declare(strict_types=1);
+
+namespace SimpleSAML\SAML2\XML\md;
+
+/**
+ * A localized name representing an organization's url.
+ *
+ * @package simplesamlphp/saml2
+ */
+final class OrganizationURL extends AbstractLocalizedName
+{
+}
