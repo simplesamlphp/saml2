e8170f50e8c8f1ca0984a494d4c75ffbb98d3d7d
Tim van Dijen <tvdijen@gmail.com>
2021-04-05 16:24:05 +0200

    Redo Audience inplementation with it's own getter/setter/toXML

--- src/SAML2/XML/saml/Audience.php
+++ src/SAML2/XML/saml/Audience.php
@@ -13,16 +13,43 @@ use SimpleSAML\XML\Exception\InvalidDOMElementException;
  *
  * @package simplesaml/saml2
  */
-final class Audience extends AbstractConditionType
+final class Audience extends AbstractSamlElement
 {
+    /** @var string */
+    protected string $value;
+
+
     /**
      * Initialize an Audience element.
      *
-     * @param string $audience
+     * @param string $value
      */
-    public function __construct(string $audience)
+    public function __construct(string $value)
     {
-        parent::__construct($audience);
+        $this->setValue($value);
+    }
+
+
+    /**
+     * Get the string value of this Audience
+     *
+     * @return string
+     */
+    public function getValue(): string
+    {
+        return $this->value;
+    }
+
+
+    /**
+     * Set the string value of this Audience
+     *
+     * @param string $value
+     */
+    protected function setValue(string $value): void
+    {
+        Assert::notWhitespaceOnly($value);
+        $this->value = $value;
     }
 
 
@@ -41,6 +68,21 @@ final class Audience extends AbstractConditionType
         Assert::same($xml->namespaceURI, Audience::NS, InvalidDOMElementException::class);
 
         return new self($xml->textContent);
+    }
+
+
+    /**
+     * Convert this Audience to XML.
+     *
+     * @param \DOMElement $parent The element we are converting to XML.
+     * @return \DOMElement The XML element after adding the data corresponding to this Condition.
+     */
+    public function toXML(DOMElement $parent = null): DOMElement
+    {
+        $element = $this->instantiateParentElement($parent);
+        $element->textContent = $this->value;
+
+        return $element;
     }
 }
 
