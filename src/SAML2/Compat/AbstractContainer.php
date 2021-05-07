c38e849dc34844001b6d5a378ef97350fda780a7
Tim van Dijen <tvdijen@gmail.com>
2021-05-07 22:35:10 +0200

    Fix namespaces

--- src/SAML2/Compat/AbstractContainer.php
+++ src/SAML2/Compat/AbstractContainer.php
@@ -7,8 +7,8 @@ namespace SimpleSAML\SAML2\Compat;
 use Psr\Log\LoggerInterface;
 use SimpleSAML\Assert\Assert;
 use SimpleSAML\XML\AbstractXMLElement;
-use SimpleSAML\XML\saml\Condition;
-use SimpleSAML\XML\saml\CustomIdentifierInterface;
+use SimpleSAML\SAML2\XML\saml\Condition;
+use SimpleSAML\SAML2\XML\saml\CustomIdentifierInterface;
 
 use function array_key_exists;
 use function is_subclass_of;
