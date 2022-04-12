6f7527e814149cc7fa8ec695d14bd92580be8cb7
Tim van Dijen <tvdijen@gmail.com>
2022-04-12 23:32:59 +0200

    Fix phpdoc

--- src/SAML2/Exception/ProtocolViolationException.php
+++ src/SAML2/Exception/ProtocolViolationException.php
@@ -5,7 +5,7 @@ declare(strict_types=1);
 namespace SimpleSAML\SAML2\Exception;
 
 /**
- * This exception may be raised when a violation of the SAML2 is detected
+ * This exception may be raised when a violation of the SAML2 specification is detected
  *
  * @package simplesamlphp/saml2
  */
