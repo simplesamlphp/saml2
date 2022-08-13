888d6c2f44b379912aae9c45a0499b29535ae50e
Tim van Dijen <tvdijen@gmail.com>
2022-08-13 22:59:12 +0200

    Migrate EncryptedID to new encryption interface

--- src/SAML2/XML/saml/EncryptedID.php
+++ src/SAML2/XML/saml/EncryptedID.php
@@ -7,6 +7,7 @@ namespace SimpleSAML\SAML2\XML\saml;
 use InvalidArgumentException;
 use SimpleSAML\SAML2\Compat\ContainerSingleton;
 use SimpleSAML\SAML2\Constants;
+use SimpleSAML\XML\DOMDocumentFactory;
 use SimpleSAML\XML\XMLElementInterface;
 use SimpleSAML\XMLSecurity\Alg\Encryption\EncryptionAlgorithmInterface;
 use SimpleSAML\XMLSecurity\Backend\EncryptionBackend;
@@ -29,6 +30,7 @@ class EncryptedID extends AbstractSamlElement implements EncryptedElementInterfa
     public function getBlacklistedAlgorithms(): ?array
     {
         // return an array with the algorithms you don't want to allow to be used
+        return [];
     }
 
 
@@ -36,6 +38,7 @@ class EncryptedID extends AbstractSamlElement implements EncryptedElementInterfa
     {
         // return the encryption backend you want to use,
         // or null if you are fine with the default
+        return null;
     }
 
 
@@ -47,24 +50,25 @@ class EncryptedID extends AbstractSamlElement implements EncryptedElementInterfa
      */
     public function decrypt(EncryptionAlgorithmInterface $decryptor): XMLElementInterface
     {
-//        $xml = Security::decryptElement($this->encryptedData->toXML(), $key, $blacklist);
-//        $id = implode(':', [$xml->namespaceURI, $xml->localName]);
-//        switch ($id) {
-//            case NameID::NS . ':NameID':
-//                return NameID::fromXML($xml);
-//            case Issuer::NS . ':Issuer':
-//                return Issuer::fromXML($xml);
-//            case BaseID::NS . ':BaseID':
-//                $xsiType = $xml->getAttributeNS(Constants::NS_XSI, 'type');
-//                $container = ContainerSingleton::getInstance();
-//                $handler = $container->getIdentifierHandler($xsiType);
-//                if ($handler !== null) {
-//                    return $handler::fromXML($xml);
-//                }
-//                return BaseID::fromXML($xml);
-//            default:
-                // Fall thru
-//        }
-//        throw new InvalidArgumentException('Unknown or unsupported encrypted identifier.');
+        $xml = DOMDocumentFactory::fromString($this->decryptData($decryptor))->documentElement;
+
+        $id = implode(':', [$xml->namespaceURI, $xml->localName]);
+        switch ($id) {
+            case NameID::NS . ':NameID':
+                return NameID::fromXML($xml);
+            case Issuer::NS . ':Issuer':
+                return Issuer::fromXML($xml);
+            case BaseID::NS . ':BaseID':
+                $xsiType = $xml->getAttributeNS(Constants::NS_XSI, 'type');
+                $container = ContainerSingleton::getInstance();
+                $handler = $container->getIdentifierHandler($xsiType);
+                if ($handler !== null) {
+                    return $handler::fromXML($xml);
+                }
+                return BaseID::fromXML($xml);
+            default:
+              // Fall thru
+        }
+        throw new InvalidArgumentException('Unknown or unsupported encrypted identifier.');
     }
 }
