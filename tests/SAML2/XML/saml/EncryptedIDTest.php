939ee581d9b90d4156dfae17098f4a72e01b8589
Tim van Dijen <tvdijen@gmail.com>
2022-08-14 17:26:48 +0200

    Migrate EncryptedIDTest to new encryption interface

--- tests/SAML2/XML/saml/EncryptedIDTest.php
+++ tests/SAML2/XML/saml/EncryptedIDTest.php
@@ -10,7 +10,7 @@ use PHPUnit\Framework\TestCase;
 use SimpleSAML\Configuration;
 use SimpleSAML\SAML2\Compat\ContainerSingleton;
 use SimpleSAML\SAML2\Compat\MockContainer;
-use SimpleSAML\SAML2\Constants;
+use SimpleSAML\SAML2\Constants as C;
 use SimpleSAML\SAML2\Utils\XPath;
 use SimpleSAML\SAML2\XML\saml\Attribute;
 use SimpleSAML\SAML2\XML\saml\BaseID;
@@ -21,6 +21,9 @@ use SimpleSAML\Test\SAML2\CustomBaseID;
 use SimpleSAML\Test\XML\SerializableXMLTestTrait;
 use SimpleSAML\XML\Chunk;
 use SimpleSAML\XML\DOMDocumentFactory;
+use SimpleSAML\XMLSecurity\Alg\KeyTransport\KeyTransportAlgorithmFactory;
+use SimpleSAML\XMLSecurity\Key\PrivateKey;
+use SimpleSAML\XMLSecurity\Key\PublicKey;
 use SimpleSAML\XMLSecurity\TestUtils\PEMCertificatesMock;
 use SimpleSAML\XMLSecurity\XML\ds\KeyInfo;
 use SimpleSAML\XMLSecurity\XML\xenc\CarriedKeyName;
@@ -31,7 +34,6 @@ use SimpleSAML\XMLSecurity\XML\xenc\EncryptedData;
 use SimpleSAML\XMLSecurity\XML\xenc\EncryptedKey;
 use SimpleSAML\XMLSecurity\XML\xenc\EncryptionMethod;
 use SimpleSAML\XMLSecurity\XML\xenc\ReferenceList;
-use SimpleSAML\XMLSecurity\XMLSecurityKey;
 
 use function dirname;
 use function strval;
@@ -48,10 +50,6 @@ final class EncryptedIDTest extends TestCase
     use SerializableXMLTestTrait;
 
 
-    /** @var \DOMDocument $retrievalMethod */
-    private DOMDocument $retrievalMethod;
-
-
     /**
      */
     public function setup(): void
@@ -60,11 +58,6 @@ final class EncryptedIDTest extends TestCase
 
         $this->xmlRepresentation = DOMDocumentFactory::fromFile(
             dirname(dirname(dirname(dirname(__FILE__)))) . '/resources/xml/saml_EncryptedID.xml'
-        );
-
-        $this->retrievalMethod = DOMDocumentFactory::fromFile(
-            dirname(dirname(dirname(dirname(dirname(__FILE__)))))
-            . '/vendor/simplesamlphp/xml-security/tests/resources/xml/ds_RetrievalMethod.xml'
         );
     }
 
@@ -132,22 +125,33 @@ final class EncryptedIDTest extends TestCase
     public function testMarshallingElementOrdering(): void
     {
         $ed = new EncryptedData(
-            new CipherData('Nk4W4mx...'),
-            'Encrypted_DATA_ID',
-            'http://www.w3.org/2001/04/xmlenc#Element',
-            "key-type",
-            'base64-encoded',
+            new CipherData(new CipherValue('iFz/8KASJCLCAHqaAKhZXWOG/TPZlgTxcQ25lTGxdSdEsGYz7cg5lfZAbcN3UITCP9MkJsyjMlRsQouIqBkoPCGZz8NXibDkQ8OUeE7JdkFgKvgUMXawp+uDL4gHR8L7l6SPAmWZU3Hx/Wg9pTJBOpTjwoS0')),
+            null,
+            null,
+            null,
+            null,
             new EncryptionMethod('http://www.w3.org/2001/04/xmlenc#aes128-cbc'),
-            new KeyInfo([new Chunk($this->retrievalMethod->documentElement)])
+            new KeyInfo([
+                new EncryptedKey(
+                    new CipherData(new CipherValue('GMhpk09X+quNC/SsnxcDglZU/DCLAu9bMJ5bPcgaBK4s3F1eXciU8hlOYNaskSwP86HmA704NbzSDOHAgN6ckR+iCssxA7XCBjz0hltsgfn5p9Rh8qKtKltiXvxo/xXTcSXXZXNcE0R2KTya0P4DjZvYYgbIls/AH8ZyDV07ntI=')),
+                    null,
+                    null,
+                    null,
+                    null,
+                    null,
+                    null,
+                    new EncryptionMethod('http://www.w3.org/2009/xmlenc11#rsa-oaep'),
+                ),
+            ]),
         );
         $ek = new EncryptedKey(
-            new CipherData('PzA5X...'),
+            new CipherData(new CipherValue('GMhpk09X+quNC/SsnxcDglZU/DCLAu9bMJ5bPcgaBK4s3F1eXciU8hlOYNaskSwP86HmA704NbzSDOHAgN6ckR+iCssxA7XCBjz0hltsgfn5p9Rh8qKtKltiXvxo/xXTcSXXZXNcE0R2KTya0P4DjZvYYgbIls/AH8ZyDV07ntI=')),
             'Encrypted_KEY_ID',
             null,
             null,
             null,
             'some_ENTITY_ID',
-            'Name of the key',
+            new CarriedKeyName('Name of the key'),
             new EncryptionMethod('http://www.w3.org/2001/04/xmlenc#rsa-1_5'),
             null,
             new ReferenceList(
@@ -155,7 +159,6 @@ final class EncryptedIDTest extends TestCase
             )
         );
         $eid = new EncryptedID($ed, [$ek]);
-
         $eidElement = $eid->toXML();
 
         // Test for an EncryptedID
@@ -163,6 +166,9 @@ final class EncryptedIDTest extends TestCase
         $eidElements = XPath::xpQuery($eidElement, './xenc:EncryptedData', $xpCache);
         $this->assertCount(1, $eidElements);
 
+        // Note: this cannot be tested as long as we don't include EncryptedKey in the structure
+        // (see simplesamlphp/xml-security issue #30)
+        //
         // Test ordering of EncryptedID contents
         /** @psalm-var \DOMElement[] $eidElements */
         $eidElements = XPath::xpQuery($eidElement, './xenc:EncryptedData/following-sibling::*', $xpCache);
@@ -176,34 +182,70 @@ final class EncryptedIDTest extends TestCase
      */
     public function testEncryption(): void
     {
+        $container = ContainerSingleton::getInstance(new MockContainer());
+        $container->setBlacklistedAlgorithms(null);
+
+        // Create keys
+        $privKey = PrivateKey::fromFile(
+            dirname(dirname(dirname(dirname(dirname(__FILE__)))))
+            . '/vendor/simplesamlphp/xml-security'
+            . PEMCertificatesMock::CERTIFICATE_DIR_RSA
+            . '/'
+            . PEMCertificatesMock::SELFSIGNED_PRIVATE_KEY
+        );
+
+        $pubKey = PublicKey::fromFile(
+            dirname(dirname(dirname(dirname(dirname(__FILE__)))))
+            . '/vendor/simplesamlphp/xml-security'
+            . PEMCertificatesMock::CERTIFICATE_DIR_RSA
+            . '/'
+            . PEMCertificatesMock::SELFSIGNED_PUBLIC_KEY
+        );
+
+        // Create encryptor
+        $encryptor = (new KeyTransportAlgorithmFactory())->getAlgorithm(
+            C::KEY_TRANSPORT_OAEP,
+            $pubKey,
+        );
+
         // test with a NameID
         $nameid = new NameID('value', 'name_qualifier');
-        $pubkey = new XMLSecurityKey(XMLSecurityKey::RSA_SHA256, ['type' => 'public']);
-        $pubkey->loadKey(PEMCertificatesMock::getPlainPublicKey(PEMCertificatesMock::PUBLIC_KEY));
-        /** \SimpleSAML\SAML2\XML\saml\AbstractSamlElement $encid */
-        $encid = EncryptedID::fromUnencryptedElement($nameid, $pubkey);
-        $str = strval($encid);
+        $encid = new EncryptedID($nameid->encrypt($encryptor));
+        $doc = DOMDocumentFactory::fromString(strval($encid));
 
-        $doc = DOMDocumentFactory::fromString($str);
-
-        /** \SimpleSAML\XMLSecurity\XML\EncryptedElementInterface $encid */
         $encid = EncryptedID::fromXML($doc->documentElement);
-        $privkey = new XMLSecurityKey(XMLSecurityKey::RSA_SHA256, ['type' => 'private']);
-        $privkey->loadKey(PEMCertificatesMock::getPlainPrivateKey(PEMCertificatesMock::PRIVATE_KEY));
-        $id = $encid->decrypt($privkey);
+        $decryptor = (new KeyTransportAlgorithmFactory())->getAlgorithm(
+            $encid->getEncryptedKey()->getEncryptionMethod()->getAlgorithm(),
+            $privKey
+        );
+        $id = $encid->decrypt($decryptor);
         $this->assertEquals(strval($nameid), strval($id));
 
         // test with Issuer
         $issuer = new Issuer('entityID');
-        $encid = EncryptedID::fromUnencryptedElement($issuer, $pubkey);
-        $id = $encid->decrypt($privkey);
+        $encid = new EncryptedID($issuer->encrypt($encryptor));
+        $doc = DOMDocumentFactory::fromString(strval($encid));
+
+        $encid = EncryptedID::fromXML($doc->documentElement);
+        $decryptor = (new KeyTransportAlgorithmFactory())->getAlgorithm(
+            $encid->getEncryptedKey()->getEncryptionMethod()->getAlgorithm(),
+            $privKey
+        );
+        $id = $encid->decrypt($decryptor);
         $this->assertInstanceOf(Issuer::class, $id);
         $this->assertEquals(strval($issuer), strval($id));
 
         // test a custom BaseID without registering it
         $customid = new CustomBaseID(1.0, 'name_qualifier');
-        $encid = EncryptedID::fromUnencryptedElement($customid, $pubkey);
-        $id = $encid->decrypt($privkey);
+        $encid = new EncryptedID($customid->encrypt($encryptor));
+        $doc = DOMDocumentFactory::fromString(strval($encid));
+
+        $encid = EncryptedID::fromXML($doc->documentElement);
+        $decryptor = (new KeyTransportAlgorithmFactory())->getAlgorithm(
+            $encid->getEncryptedKey()->getEncryptionMethod()->getAlgorithm(),
+            $privKey
+        );
+        $id = $encid->decrypt($decryptor);
         $this->assertInstanceOf(BaseID::class, $id);
         $this->assertEquals(strval($customid), strval($id));
 
@@ -212,16 +254,21 @@ final class EncryptedIDTest extends TestCase
         $container->method('getIdentifierHandler')->willReturn(CustomBaseID::class);
         ContainerSingleton::setContainer($container);
 
-        $encid = EncryptedID::fromUnencryptedElement($customid, $pubkey);
-        $id = $encid->decrypt($privkey);
+        $encid = EncryptedID::fromXML($doc->documentElement);
+        $decryptor = (new KeyTransportAlgorithmFactory())->getAlgorithm(
+            $encid->getEncryptedKey()->getEncryptionMethod()->getAlgorithm(),
+            $privKey
+        );
+        $id = $encid->decrypt($decryptor);
         $this->assertInstanceOf(CustomBaseID::class, $id);
         $this->assertEquals(strval($customid), strval($id));
 
         // test with unsupported ID
         $attr = new Attribute('name');
-        $encid = EncryptedID::fromUnencryptedElement($attr, $pubkey);
+        $encid = new EncryptedID($attr->encrypt($encryptor));
+
         $this->expectException(InvalidArgumentException::class);
         $this->expectExceptionMessage('Unknown or unsupported encrypted identifier.');
-        $encid->decrypt($privkey);
+        $encid->decrypt($decryptor);
     }
 }
