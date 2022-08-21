bdb6a46c80c887a50fdfaef656606472410687de
Tim van Dijen <tvdijen@gmail.com>
2022-08-21 20:32:59 +0200

    Drop unnecessary constructor

--- src/SAML2/XML/md/EncryptionMethod.php
+++ src/SAML2/XML/md/EncryptionMethod.php
@@ -19,22 +19,4 @@ class EncryptionMethod extends AbstractEncryptionMethod
 
     /** @var string */
     public const NS_PREFIX = 'md';
-
-
-    /*
-     * EncryptionMethod constructor.
-     *
-     * @param string $algorithm
-     * @param int|null $keySize
-     * @param string|null $oaepParams
-     * @param \SimpleSAML\XML\Chunk[] $children
-     */
-    public function __construct(
-        string $algorithm,
-        ?int $keySize = null,
-        ?string $oaepParams = null,
-        array $children = []
-    ) {
-        parent::__construct($algorithm, $keySize, $oaepParams, $children);
-    }
 }
