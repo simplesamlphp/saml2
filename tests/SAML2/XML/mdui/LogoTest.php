d263bfc4a6f356a39ca8e84fec966f352cd69688
Tim van Dijen <tvdijen@gmail.com>
2022-10-05 18:49:36 +0200

    Test unmarshalling with no language set

--- tests/SAML2/XML/mdui/LogoTest.php
+++ tests/SAML2/XML/mdui/LogoTest.php
@@ -87,6 +87,22 @@ IMG;
 
 
     /**
+     * Unmarshalling of a logo tag without a language
+     */
+    public function testUnmarshallingWithoutLanguage(): void
+    {
+        $xmlRepresentation = $this->xmlRepresentation->documentElement;
+        $xmlRepresentation->removeAttribute('xml:lang');
+
+        $logo = Logo::fromXML($xmlRepresentation);
+        $this->assertNull($logo->getLanguage());
+        $this->assertEquals(200, $logo->getHeight());
+        $this->assertEquals(300, $logo->getWidth());
+        $this->assertEquals($this->url, $logo->getContent());
+    }
+
+
+    /**
      * Unmarshalling of a logo tag with a data: URL
      */
     public function testUnmarshallingDataURL(): void
