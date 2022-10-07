be1689052649b6e04577edcd5115ac139606a23d
Tim van Dijen <tvdijen@gmail.com>
2022-10-07 19:17:46 +0200

    Add interoperability workflow; test eduGAIN metadata

---
+++ tests/InterOperability/EntitiesDescriptorTest.php
@@ -0,0 +1,50 @@
+<?php
+
+declare(strict_types=1);
+
+namespace SimpleSAML\Test\SAML2;
+
+use DOMDocument;
+use DOMElement;
+use PHPUnit\Framework\TestCase;
+use SimpleSAML\Assert\Assert;
+use SimpleSAML\Assert\AssertionFailedException;
+use SimpleSAML\SAML2\XML\md\EntitiesDescriptor;
+use SimpleSAML\XML\DOMDocumentFactory;
+
+/**
+ * Class \SimpleSAML\SAML2\EntitiesDescriptorTest
+ *
+ * @package simplesamlphp\saml2
+ */
+final class EntitiesDescriptorTest extends TestCase
+{
+    /**
+     * @dataProvider provideMetadata
+     * @param boolean $shouldPass
+     * @param \DOMElement $metadata;
+     */
+    public function testUnmarshalling(bool $shouldPass, DOMElement $metadata): void
+    {
+        try {
+            EntitiesDescriptor::fromXML($metadata);
+            $this->assertTrue($shouldPass);
+        } catch (AssertionFailedException $e) {
+            $this->assertFalse($shouldPass);
+        }
+    }
+
+
+    /**
+     * @return array
+     */
+    public function provideMetadata(): array
+    {
+        return [
+            'eduGAIN' => [
+                true,
+                DOMDocumentFactory::fromFile('/tmp/edugain.xml')->documentElement,
+            ],
+        ];
+    }
+}
