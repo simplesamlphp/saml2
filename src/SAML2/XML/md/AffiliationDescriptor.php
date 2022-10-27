e8b7e6ed2d4840861762c7ca6e2c59c553af69f8
Tim van Dijen <tvdijen@gmail.com>
2022-10-27 19:00:00 +0200

    Fix typo

--- src/SAML2/XML/md/AffiliationDescriptor.php
+++ src/SAML2/XML/md/AffiliationDescriptor.php
@@ -8,7 +8,7 @@ use DOMElement;
 use Exception;
 use SimpleSAML\Assert\Assert;
 use SimpleSAML\SAML2\Constants as C;
-use SimpleSAML\SAML2\Exception\ProtovolViolationException;
+use SimpleSAML\SAML2\Exception\ProtocolViolationException;
 use SimpleSAML\XML\Exception\InvalidDOMElementException;
 use SimpleSAML\XML\Exception\SchemaViolationException;
 use SimpleSAML\XML\Exception\TooManyElementsException;
