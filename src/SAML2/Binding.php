e6691942ffd7a60cf0e8bce15d6f0864209e61f5
Thijs Kinkhorst <thijs@kinkhorst.com>
2022-11-23 13:04:40 +0100

    Fix/extend binding tests

--- src/SAML2/Binding.php
+++ src/SAML2/Binding.php
@@ -94,7 +94,7 @@ abstract class Binding
                      */
                     ($contentType === 'text/xml' || $contentType === 'application/xml')
                     // See paragraph 3.2.3 of Binding for SAML2 (OASIS)
-                    || $_SERVER['HTTP_SOAPACTION'] === 'http://www.oasis-open.org/committees/security')
+                    || (isset($_SERVER['HTTP_SOAPACTION']) && $_SERVER['HTTP_SOAPACTION'] === 'http://www.oasis-open.org/committees/security'))
                 {
                     return new SOAP();
                 }
