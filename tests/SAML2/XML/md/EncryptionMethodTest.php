b41cd76267dffbd798bdde4afe01461f54be9026
Tim van Dijen <tim.dijen@minbzk.nl>
2023-01-18 11:14:13 +0100

    Reflect changes in xml-security v1.1

--- tests/SAML2/XML/md/EncryptionMethodTest.php
+++ tests/SAML2/XML/md/EncryptionMethodTest.php
@@ -162,7 +162,7 @@ XML
         $em = EncryptionMethod::fromXML($document->documentElement);
         $this->assertNull($em->getKeySize());
         $this->assertNull($em->getOAEPParams());
-        $this->assertEmpty($em->getChildren());
+        $this->assertEmpty($em->getElements());
         $this->assertEquals(
             $document->saveXML($document->documentElement),
             strval($em)
