f762a990a987fffbbc96e97a065098c52262c440
Tim van Dijen <tvdijen@gmail.com>
2023-01-31 20:45:53 +0100

    Implement AssertionURIRef element

---
+++ src/SAML2/XML/saml/AssertionURIRef.php
@@ -0,0 +1,76 @@
+<?php
+
+declare(strict_types=1);
+
+namespace SimpleSAML\SAML2\XML\saml;
+
+use DOMElement;
+use SimpleSAML\Assert\Assert;
+use SimpleSAML\XML\Exception\InvalidDOMElementException;
+use SimpleSAML\XML\Exception\SchemaViolationException;
+use SimpleSAML\XML\StringElementTrait;
+
+/**
+ * Class representing a saml:AssertionURIRef element.
+ *
+ * @package simplesaml/saml2
+ */
+final class AssertionURIRef extends AbstractSamlElement
+{
+    use StringElementTrait;
+
+
+    /**
+     * @param string $content
+     */
+    public function __construct(string $content)
+    {
+        $this->setContent($content);
+    }
+
+
+    /**
+     * Validate the content of the element.
+     *
+     * @param string $content  The value to go in the XML textContent
+     * @throws \Exception on failure
+     * @return void
+     */
+    protected function validateContent(string $content): void
+    {
+        Assert::validURI($content, SchemaViolationException::class); // Covers the empty string
+    }
+
+
+    /**
+     * Convert XML into an AssertionURIRef
+     *
+     * @param \DOMElement $xml The XML element we should load
+     * @return static
+     *
+     * @throws \SimpleSAML\XML\Exception\InvalidDOMElementException
+     *   If the qualified name of the supplied element is wrong
+     */
+    public static function fromXML(DOMElement $xml): static
+    {
+        Assert::same($xml->localName, 'AssertionURIRef', InvalidDOMElementException::class);
+        Assert::same($xml->namespaceURI, AssertionURIRef::NS, InvalidDOMElementException::class);
+
+        return new static($xml->textContent);
+    }
+
+
+    /**
+     * Convert this AssertionURIRef to XML.
+     *
+     * @param \DOMElement $parent The element we are converting to XML.
+     * @return \DOMElement The XML element after adding the data corresponding to this Condition.
+     */
+    public function toXML(DOMElement $parent = null): DOMElement
+    {
+        $element = $this->instantiateParentElement($parent);
+        $element->textContent = $this->getContent();
+
+        return $element;
+    }
+}
