217d89c8db3a4cfa45621505cfc634f75ab371c0
Tim van Dijen <tvdijen@gmail.com>
2023-01-31 22:28:36 +0100

    Implement saml:Evidence element

---
+++ src/SAML2/XML/saml/Evidence.php
@@ -0,0 +1,14 @@
+<?php
+
+declare(strict_types=1);
+
+namespace SimpleSAML\SAML2\XML\saml;
+
+/**
+ * Class representing a saml:Evidence element.
+ *
+ * @package simplesaml/saml2
+ */
+final class Evidence extends AbstractEvidenceType
+{
+}
