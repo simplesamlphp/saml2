91ab10fccf7de47fe2d5a5ead6f1268455d0e542
Tim van Dijen <tvdijen@gmail.com>
2023-02-08 23:04:56 +0100

    Implement samlp:AsserionIDRequest element (#323)
    * Implement samlp:AsserionIDRequest element

--- src/SAML2/XML/samlp/AbstractMessage.php
+++ src/SAML2/XML/samlp/AbstractMessage.php
@@ -64,6 +64,7 @@ abstract class AbstractMessage extends AbstractSamlpElement implements SignableE
      * The original signed XML
      *
      * @var \DOMElement
+     * @psalm-suppress PropertyNotSetInConstructor
      */
     protected DOMElement $xml;
 
@@ -85,7 +86,7 @@ abstract class AbstractMessage extends AbstractSamlpElement implements SignableE
     protected function __construct(
         protected ?Issuer $issuer = null,
         protected ?string $id = null,
-        protected $version = '2.0',
+        protected string $version = '2.0',
         protected ?int $issueInstant = null,
         protected ?string $destination = null,
         protected ?string $consent = null,
