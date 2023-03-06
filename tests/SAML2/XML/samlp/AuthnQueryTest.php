3c21e71e8b7b7f26ad663721f1f648a9dcbc6a62
Tim van Dijen <tvdijen@gmail.com>
2023-03-06 22:37:13 +0100

    Add element samlp:AuthnQuery

---
+++ tests/SAML2/XML/samlp/AuthnQueryTest.php
@@ -0,0 +1,95 @@
+<?php
+
+declare(strict_types=1);
+
+namespace SimpleSAML\Test\SAML2\XML\samlp;
+
+use DOMDocument;
+use PHPUnit\Framework\TestCase;
+use SimpleSAML\SAML2\Constants as C;
+use SimpleSAML\SAML2\Utils\XPath;
+use SimpleSAML\SAML2\XML\saml\AuthnContextDeclRef;
+use SimpleSAML\SAML2\XML\saml\Issuer;
+use SimpleSAML\SAML2\XML\saml\NameID;
+use SimpleSAML\SAML2\XML\saml\Subject;
+use SimpleSAML\SAML2\XML\samlp\AuthnQuery;
+use SimpleSAML\SAML2\XML\samlp\RequestedAuthnContext;
+use SimpleSAML\Test\XML\SchemaValidationTestTrait;
+use SimpleSAML\Test\XML\SerializableElementTestTrait;
+use SimpleSAML\XML\DOMDocumentFactory;
+use SimpleSAML\XML\Exception\MissingElementException;
+use SimpleSAML\XML\Exception\MissingAttributeException;
+use SimpleSAML\XML\Exception\TooManyElementsException;
+use SimpleSAML\XMLSecurity\TestUtils\SignedElementTestTrait;
+
+use function dirname;
+use function strval;
+
+/**
+ * Class \SimpleSAML\SAML2\XML\samlp\AuthnQueryTest
+ *
+ * @covers \SimpleSAML\SAML2\XML\samlp\AuthnQuery
+ * @covers \SimpleSAML\SAML2\XML\samlp\AbstractSubjectQuery
+ * @covers \SimpleSAML\SAML2\XML\samlp\AbstractRequest
+ * @covers \SimpleSAML\SAML2\XML\samlp\AbstractMessage
+ * @covers \SimpleSAML\SAML2\XML\samlp\AbstractSamlpElement
+ * @package simplesamlphp/saml2
+ */
+final class AuthnQueryTest extends TestCase
+{
+    use SchemaValidationTestTrait;
+    use SerializableElementTestTrait;
+    use SignedElementTestTrait;
+
+
+    /**
+     */
+    public function setup(): void
+    {
+        $this->schema = dirname(__FILE__, 5) . '/schemas/saml-schema-protocol-2.0.xsd';
+
+        $this->testedClass = AuthnQuery::class;
+
+        $this->xmlRepresentation = DOMDocumentFactory::fromFile(
+            dirname(__FILE__, 4) . '/resources/xml/samlp_AuthnQuery.xml',
+        );
+    }
+
+
+    /**
+     */
+    public function testMarshalling(): void
+    {
+        $nameId = new NameID('urn:example:subject', null, null, C::NAMEID_UNSPECIFIED);
+        $authnContextDeclRef = new AuthnContextDeclRef('https://example.org/relative/path/to/document.xml');
+        $requestedAuthnContext = new RequestedAuthnContext([$authnContextDeclRef], 'exact');
+
+        $authnQuery = new AuthnQuery(
+            subject: new Subject($nameId),
+            requestedAuthnContext: $requestedAuthnContext,
+            issuer: new Issuer(
+                value: 'https://example.org/',
+                Format: C::NAMEID_ENTITY,
+            ),
+            id: 'aaf23196-1773-2113-474a-fe114412ab72',
+            issueInstant: 1504698567,
+            sessionIndex: 'phpunit',
+        );
+
+        $this->assertEquals(
+            $this->xmlRepresentation->saveXML($this->xmlRepresentation->documentElement),
+            strval($authnQuery),
+        );
+    }
+
+
+    public function testUnmarshalling(): void
+    {
+        $authnQuery = AuthnQuery::fromXML($this->xmlRepresentation->documentElement);
+
+        $this->assertEquals(
+            $this->xmlRepresentation->saveXML($this->xmlRepresentation->documentElement),
+            strval($authnQuery),
+        );
+    }
+}
