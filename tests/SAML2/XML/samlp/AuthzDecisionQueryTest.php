d1976332813267c5b13e223f5730e220276a1915
Tim van Dijen <tvdijen@gmail.com>
2023-04-24 22:56:21 +0200

    Add samlp:AuthzDecisionQuery

---
+++ tests/SAML2/XML/samlp/AuthzDecisionQueryTest.php
@@ -0,0 +1,101 @@
+<?php
+
+declare(strict_types=1);
+
+namespace SimpleSAML\Test\SAML2\XML\samlp;
+
+use DOMDocument;
+use PHPUnit\Framework\TestCase;
+use SimpleSAML\Test\SAML2\Constants as C;
+use SimpleSAML\SAML2\XML\saml\Action;
+use SimpleSAML\SAML2\XML\saml\Assertion;
+use SimpleSAML\SAML2\XML\saml\Evidence;
+use SimpleSAML\SAML2\XML\saml\Issuer;
+use SimpleSAML\SAML2\XML\saml\NameID;
+use SimpleSAML\SAML2\XML\saml\Subject;
+use SimpleSAML\SAML2\XML\samlp\AuthzDecisionQuery;
+use SimpleSAML\XML\DOMDocumentFactory;
+use SimpleSAML\XML\TestUtils\SchemaValidationTestTrait;
+use SimpleSAML\XML\TestUtils\SerializableElementTestTrait;
+use SimpleSAML\XMLSecurity\TestUtils\SignedElementTestTrait;
+
+use function dirname;
+use function strval;
+
+/**
+ * Class \SimpleSAML\SAML2\XML\samlp\AuthzDecisionQueryTest
+ *
+ * @covers \SimpleSAML\SAML2\XML\samlp\AuthzDecisionQuery
+ * @covers \SimpleSAML\SAML2\XML\samlp\AbstractSubjectQuery
+ * @covers \SimpleSAML\SAML2\XML\samlp\AbstractRequest
+ * @covers \SimpleSAML\SAML2\XML\samlp\AbstractMessage
+ * @covers \SimpleSAML\SAML2\XML\samlp\AbstractSamlpElement
+ * @package simplesamlphp/saml2
+ */
+final class AuthzDecisionQueryTest extends TestCase
+{
+    use SchemaValidationTestTrait;
+    use SerializableElementTestTrait;
+    use SignedElementTestTrait;
+
+
+    /**
+     */
+    public function setup(): void
+    {
+        $this->schema = dirname(__FILE__, 5) . '/resources/schemas/saml-schema-protocol-2.0.xsd';
+
+        $this->testedClass = AuthzDecisionQuery::class;
+
+        $this->xmlRepresentation = DOMDocumentFactory::fromFile(
+            dirname(__FILE__, 4) . '/resources/xml/samlp_AuthzDecisionQuery.xml',
+        );
+
+        $this->assertion = DOMDocumentFactory::fromFile(
+            dirname(__FILE__, 4) . '/resources/xml/saml_Assertion.xml',
+        );
+    }
+
+
+    /**
+     */
+    public function testMarshalling(): void
+    {
+        $nameId = new NameID('urn:example:subject', null, null, C::NAMEID_UNSPECIFIED);
+        $evidence = new Evidence(
+            assertion: [Assertion::fromXML($this->assertion->documentElement)],
+        );
+
+        $authzDecisionQuery = new AuthzDecisionQuery(
+            subject: new Subject($nameId),
+            resource: 'urn:x-simplesamlphp:resource',
+            action: [
+                new Action(C::NAMESPACE, 'SomeAction'),
+                new Action('urn:x-simplesamlphp:alt-namespace', 'SomeOtherAction'),
+            ],
+            evidence: $evidence,
+            issuer: new Issuer(
+                value: 'https://example.org/',
+                Format: C::NAMEID_ENTITY,
+            ),
+            id: 'aaf23196-1773-2113-474a-fe114412ab72',
+            issueInstant: 1504698567,
+        );
+
+        $this->assertEquals(
+            $this->xmlRepresentation->saveXML($this->xmlRepresentation->documentElement),
+            strval($authzDecisionQuery),
+        );
+    }
+
+
+    public function testUnmarshalling(): void
+    {
+        $authzDecisionQuery = AuthzDecisionQuery::fromXML($this->xmlRepresentation->documentElement);
+
+        $this->assertEquals(
+            $this->xmlRepresentation->saveXML($this->xmlRepresentation->documentElement),
+            strval($authzDecisionQuery),
+        );
+    }
+}
