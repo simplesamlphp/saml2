f608e45087cc38e5c52b7b4235713b454ff33582
Tim van Dijen <tvdijen@gmail.com>
2023-06-03 17:08:34 +0200

    Add ecp:RelayState

---
+++ src/SAML2/XML/ecp/RelayState.php
@@ -0,0 +1,118 @@
+<?php
+
+declare(strict_types=1);
+
+namespace SimpleSAML\SAML2\XML\ecp;
+
+use DOMElement;
+use SimpleSAML\Assert\Assert;
+use SimpleSAML\SAML2\Exception\ProtocolViolationException;
+use SimpleSAML\SOAP\Constants as C;
+use SimpleSAML\XML\Exception\InvalidDOMElementException;
+use SimpleSAML\XML\Exception\MissingAttributeException;
+use SimpleSAML\XML\Exception\TooManyElementsException;
+use SimpleSAML\XML\Exception\SchemaViolationException;
+use SimpleSAML\XML\StringElementTrait;
+
+use function boolval;
+use function intval;
+use function strval;
+
+/**
+ * Class representing the ECP RelayState element.
+ *
+ * @package simplesamlphp/saml2
+ */
+final class RelayState extends AbstractEcpElement
+{
+    use StringElementTrait;
+
+    /**
+     * Create a ECP RelayState element.
+     *
+     * @param bool $mustUnderstand
+     * @param string $relayState
+     */
+    public function __construct(
+        protected bool $mustUnderstand,
+        string $relayState,
+    ) {
+        $this->setContent($relayState);
+    }
+
+
+    /**
+     * Collect the value of the mustUnderstand-property
+     *
+     * @return bool
+     */
+    public function getMustUnderstand(): bool
+    {
+        return $this->mustUnderstand;
+    }
+
+
+    /**
+     * Convert XML into a RelayState
+     *
+     * @param \DOMElement $xml The XML element we should load
+     * @return static
+     *
+     * @throws \SimpleSAML\XML\Exception\InvalidDOMElementException
+     *   if the qualified name of the supplied element is wrong
+     * @throws \SimpleSAML\XML\Exception\MissingAttributeException
+     *   if the supplied element is missing any of the mandatory attributes
+     */
+    public static function fromXML(DOMElement $xml): static
+    {
+        Assert::same($xml->localName, 'RelayState', InvalidDOMElementException::class);
+        Assert::same($xml->namespaceURI, RelayState::NS, InvalidDOMElementException::class);
+
+        // Assert required attributes
+        Assert::true(
+            $xml->hasAttributeNS(C::NS_SOAP_ENV_11, 'actor'),
+            'Missing env:actor attribute in <ecp:RelayState>.',
+            MissingAttributeException::class,
+        );
+        Assert::true(
+            $xml->hasAttributeNS(C::NS_SOAP_ENV_11, 'mustUnderstand'),
+            'Missing env:mustUnderstand attribute in <ecp:RelayState>.',
+            MissingAttributeException::class,
+        );
+
+        $mustUnderstand = $xml->getAttributeNS(C::NS_SOAP_ENV_11, 'mustUnderstand');
+        $mustUnderstand = ($mustUnderstand === '') ? null : boolval(intval($mustUnderstand));
+        Assert::nullOrBoolean(
+            $mustUnderstand,
+            'Invalid value of env:mustUnderstand attribute in <ecp:RelayState>.',
+            ProtocolViolationException::class,
+        );
+
+        $actor = $xml->getAttributeNS(C::NS_SOAP_ENV_11, 'actor');
+        Assert::same(
+            $actor,
+            C::SOAP_ACTOR_NEXT,
+            'Invalid value of env:actor attribute in <ecp:RelayState>.',
+            ProtocolViolationException::class,
+        );
+
+        return new static($mustUnderstand, $xml->textContent);
+    }
+
+
+    /**
+     * Convert this ECP RelayState to XML.
+     *
+     * @param \DOMElement|null $parent The element we should append this element to.
+     * @return \DOMElement
+     */
+    public function toXML(DOMElement $parent = null): DOMElement
+    {
+        $e = $this->instantiateParentElement($parent);
+        $e->setAttributeNS(C::NS_SOAP_ENV_11, 'env:mustUnderstand', strval($this->getMustUnderstand()));
+        $e->setAttributeNS(C::NS_SOAP_ENV_11, 'env:actor', C::SOAP_ACTOR_NEXT);
+        $e->textContent = $this->getContent();
+
+        return $e;
+    }
+}
---
+++ tests/SAML2/XML/ecp/RelayStateTest.php
@@ -0,0 +1,101 @@
+<?php
+
+declare(strict_types=1);
+
+namespace SimpleSAML\Test\SAML2\XML\ecp;
+
+use DOMDocument;
+use DOMElement;
+use PHPUnit\Framework\TestCase;
+use SimpleSAML\SAML2\Exception\ProtocolViolationException;
+use SimpleSAML\SAML2\XML\ecp\RelayState;
+use SimpleSAML\SOAP\Constants as SOAP;
+use SimpleSAML\Test\SAML2\Constants as C;
+use SimpleSAML\XML\Chunk;
+use SimpleSAML\XML\DOMDocumentFactory;
+use SimpleSAML\XML\Exception\MissingAttributeException;
+use SimpleSAML\XML\Exception\SchemaViolationException;
+use SimpleSAML\XML\TestUtils\SchemaValidationTestTrait;
+use SimpleSAML\XML\TestUtils\SerializableElementTestTrait;
+
+use function dirname;
+use function strval;
+
+/**
+ * @covers \SimpleSAML\SAML2\XML\ecp\AbstractEcpElement
+ * @covers \SimpleSAML\SAML2\XML\ecp\RelayState
+ * @package simplesamlphp/saml2
+ */
+final class RelayStateTest extends TestCase
+{
+    use SchemaValidationTestTrait;
+    use SerializableElementTestTrait;
+
+
+    /**
+     */
+    public static function setUpBeforeClass(): void
+    {
+        self::$schemaFile = dirname(__FILE__, 5) . '/resources/schemas/saml-schema-ecp-2.0.xsd';
+
+        self::$testedClass = RelayState::class;
+
+        self::$xmlRepresentation = DOMDocumentFactory::fromFile(
+            dirname(__FILE__, 4) . '/resources/xml/ecp_RelayState.xml'
+        );
+    }
+
+
+    /**
+     */
+    public function testMarshalling(): void
+    {
+        $relayState = new RelayState(true, 'AGDY854379dskssda');
+
+        $this->assertEquals(
+            self::$xmlRepresentation->saveXML(self::$xmlRepresentation->documentElement),
+            strval($relayState),
+        );
+    }
+
+
+    /**
+     */
+    public function testUnmarshalling(): void
+    {
+        $relayState = RelayState::fromXML(self::$xmlRepresentation->documentElement);
+
+        $this->assertEquals(
+            self::$xmlRepresentation->saveXML(self::$xmlRepresentation->documentElement),
+            strval($relayState),
+        );
+    }
+
+
+    /**
+     */
+    public function testUnmarshallingWithMissingMustUnderstandThrowsException(): void
+    {
+        $document = clone self::$xmlRepresentation->documentElement;
+        $document->removeAttributeNS(SOAP::NS_SOAP_ENV_11, 'mustUnderstand');
+
+        $this->expectException(MissingAttributeException::class);
+        $this->expectExceptionMessage('Missing env:mustUnderstand attribute in <ecp:RelayState>.');
+
+        RelayState::fromXML($document);
+    }
+
+
+    /**
+     */
+    public function testUnmarshallingWithMissingActorThrowsException(): void
+    {
+        $document = clone self::$xmlRepresentation->documentElement;
+        $document->removeAttributeNS(SOAP::NS_SOAP_ENV_11, 'actor');
+
+        $this->expectException(MissingAttributeException::class);
+        $this->expectExceptionMessage('Missing env:actor attribute in <ecp:RelayState>.');
+
+        RelayState::fromXML($document);
+    }
+}
