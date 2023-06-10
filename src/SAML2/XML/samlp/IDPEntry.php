50e914481fbd86d098ea400c91d734c8e90a5793
Tim van Dijen <tvdijen@gmail.com>
2023-06-10 21:46:52 +0200

    Backport samlp:IDPEntry

---
+++ src/SAML2/XML/samlp/IDPEntry.php
@@ -0,0 +1,109 @@
+<?php
+
+declare(strict_types=1);
+
+namespace SimpleSAML\SAML2\XML\samlp;
+
+use DOMElement;
+use SimpleSAML\Assert\Assert;
+use SimpleSAML\XML\Exception\InvalidDOMElementException;
+use SimpleSAML\XML\Exception\SchemaViolationException;
+
+/**
+ * Class for handling SAML2 IDPEntry.
+ *
+ * @package simplesamlphp/saml2
+ */
+final class IDPEntry extends AbstractSamlpElement
+{
+    /**
+     * Initialize an IDPEntry element.
+     *
+     * @param string $providerId
+     * @param string|null $name
+     * @param string|null $loc
+     */
+    public function __construct(
+        protected string $providerId,
+        protected ?string $name = null,
+        protected ?string $loc = null,
+    ) {
+        Assert::validURI($providerId, SchemaViolationException::class); // Covers the empty string
+        Assert::nullOrNotWhitespaceOnly($name);
+        Assert::nullOrValidURI($loc, SchemaViolationException::class); // Covers the empty string
+    }
+
+
+    /**
+     * @return string
+     */
+    public function getProviderId(): string
+    {
+        return $this->providerId;
+    }
+
+
+    /**
+     * @return string|null
+     */
+    public function getName(): ?string
+    {
+        return $this->name;
+    }
+
+
+    /**
+     * @return string|null
+     */
+    public function getLoc(): ?string
+    {
+        return $this->loc;
+    }
+
+
+    /**
+     * Convert XML into a IDPEntry-element
+     *
+     * @param \DOMElement $xml The XML element we should load
+     * @return static
+     *
+     * @throws \SimpleSAML\XML\Exception\InvalidDOMElementException
+     *   if the qualified name of the supplied element is wrong
+     * @throws \SimpleSAML\XML\Exception\MissingAttributeException
+     *   if the supplied element is missing one of the mandatory attributes
+     */
+    public static function fromXML(DOMElement $xml): static
+    {
+        Assert::same($xml->localName, 'IDPEntry', InvalidDOMElementException::class);
+        Assert::same($xml->namespaceURI, IDPEntry::NS, InvalidDOMElementException::class);
+
+        $providerId = self::getAttribute($xml, 'ProviderID');
+        $name = self::getOptionalAttribute($xml, 'Name', null);
+        $loc = self::getOptionalAttribute($xml, 'Loc', null);
+
+        return new static($providerId, $name, $loc);
+    }
+
+
+    /**
+     * Convert this IDPEntry to XML.
+     *
+     * @param \DOMElement|null $parent The element we should append this IDPEntry to.
+     * @return \DOMElement
+     */
+    public function toXML(DOMElement $parent = null): DOMElement
+    {
+        $e = $this->instantiateParentElement($parent);
+        $e->setAttribute('ProviderID', $this->getProviderId());
+
+        if ($this->getName() !== null) {
+            $e->setAttribute('Name', $this->getName());
+        }
+
+        if ($this->getLoc() !== null) {
+            $e->setAttribute('Loc', $this->getLoc());
+        }
+
+        return $e;
+    }
+}
---
+++ tests/SAML2/XML/samlp/IDPEntryTest.php
@@ -0,0 +1,90 @@
+<?php
+
+declare(strict_types=1);
+
+namespace SimpleSAML\Test\SAML2\XML\samlp;
+
+use DOMDocument;
+use PHPUnit\Framework\TestCase;
+use SimpleSAML\SAML2\XML\samlp\IDPEntry;
+use SimpleSAML\XML\DOMDocumentFactory;
+use SimpleSAML\XML\TestUtils\SchemaValidationTestTrait;
+use SimpleSAML\XML\TestUtils\SerializableElementTestTrait;
+
+use function dirname;
+use function strval;
+
+/**
+ * Class \SAML2\XML\samlp\IDPEntryTest
+ *
+ * @covers \SimpleSAML\SAML2\XML\samlp\IDPEntry
+ * @covers \SimpleSAML\SAML2\XML\samlp\AbstractSamlpElement
+ *
+ * @package simplesamlphp/saml2
+ */
+final class IDPEntryTest extends TestCase
+{
+    use SchemaValidationTestTrait;
+    use SerializableElementTestTrait;
+
+
+    /**
+     */
+    public static function setUpBeforeClass(): void
+    {
+        self::$schemaFile = dirname(__FILE__, 5) . '/resources/schemas/saml-schema-protocol-2.0.xsd';
+
+        self::$testedClass = IDPentry::class;
+
+        self::$xmlRepresentation = DOMDocumentFactory::fromFile(
+            dirname(__FILE__, 4) . '/resources/xml/samlp_IDPEntry.xml',
+        );
+    }
+
+
+    /**
+     */
+    public function testMarshalling(): void
+    {
+        $entry = new IDPEntry('urn:some:requester', 'testName', 'urn:test:testLoc');
+
+        $this->assertEquals(
+            self::$xmlRepresentation->saveXML(self::$xmlRepresentation->documentElement),
+            strval($entry),
+        );
+    }
+
+
+    /**
+     */
+    public function testMarshallingNullables(): void
+    {
+        $document = clone self::$xmlRepresentation;
+        $document->documentElement->removeAttribute('Name');
+        $document->documentElement->removeAttribute('Loc');
+
+        $entry = new IDPEntry('urn:some:requester');
+
+        $this->assertEquals('urn:some:requester', $entry->getProviderID());
+        $this->assertNull($entry->getName());
+        $this->assertNull($entry->getLoc());
+
+        $this->assertEquals(
+            $document->saveXML($document->documentElement),
+            strval($entry),
+        );
+    }
+
+
+    /**
+     */
+    public function testUnmarshalling(): void
+    {
+        $entry = IDPEntry::fromXML(self::$xmlRepresentation->documentElement);
+
+        $this->assertEquals(
+            self::$xmlRepresentation->saveXML(self::$xmlRepresentation->documentElement),
+            strval($entry),
+        );
+    }
+}
