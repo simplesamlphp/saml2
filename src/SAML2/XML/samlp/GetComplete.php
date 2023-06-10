ca4fc61cbc46d2f3a782c3b02c112d5a2c9b534a
Tim van Dijen <tvdijen@gmail.com>
2023-06-10 21:42:07 +0200

    Backport samlp:GetComplete

---
+++ src/SAML2/XML/samlp/GetComplete.php
@@ -0,0 +1,61 @@
+<?php
+
+declare(strict_types=1);
+
+namespace SimpleSAML\SAML2\XML\samlp;
+
+use DOMElement;
+use SimpleSAML\Assert\Assert;
+use SimpleSAML\XML\Exception\InvalidDOMElementException;
+use SimpleSAML\XML\Exception\SchemaViolationException;
+use SimpleSAML\XML\StringElementTrait;
+
+/**
+ * Class representing a samlp:GetComplete element.
+ *
+ * @package simplesaml/saml2
+ */
+final class GetComplete extends AbstractSamlpElement
+{
+    use StringElementTrait;
+
+
+    /**
+     * @param string $content
+     */
+    public function __construct(string $content)
+    {
+        $this->setContent($content);
+    }
+
+
+    /**
+     * Validate the content of the element.
+     *
+     * @param string $content  The value to go in the XML textContent
+     * @throws \Exception on failure
+     * @return void
+     */
+    protected function validateContent(string $content): void
+    {
+        Assert::validURI($content, SchemaViolationException::class); // Covers the empty string
+    }
+
+
+    /**
+     * Convert XML into an GetComplete
+     *
+     * @param \DOMElement $xml The XML element we should load
+     * @return static
+     *
+     * @throws \SimpleSAML\XML\Exception\InvalidDOMElementException
+     *   If the qualified name of the supplied element is wrong
+     */
+    public static function fromXML(DOMElement $xml): static
+    {
+        Assert::same($xml->localName, 'GetComplete', InvalidDOMElementException::class);
+        Assert::same($xml->namespaceURI, GetComplete::NS, InvalidDOMElementException::class);
+
+        return new static($xml->textContent);
+    }
+}
---
+++ tests/SAML2/XML/samlp/GetCompleteTest.php
@@ -0,0 +1,71 @@
+<?php
+
+declare(strict_types=1);
+
+namespace SimpleSAML\SAML2\Test\SAML2\XML\samlp;
+
+use DOMDocument;
+use PHPUnit\Framework\TestCase;
+use SimpleSAML\SAML2\XML\samlp\GetComplete;
+use SimpleSAML\XML\DOMDocumentFactory;
+use SimpleSAML\XML\TestUtils\SchemaValidationTestTrait;
+use SimpleSAML\XML\TestUtils\SerializableElementTestTrait;
+
+use function dirname;
+use function strval;
+
+/**
+ * Class \SimpleSAML\SAML2\XML\samlp\GetCompleteTest
+ *
+ * @covers \SimpleSAML\SAML2\XML\samlp\GetComplete
+ * @covers \SimpleSAML\SAML2\XML\samlp\AbstractSamlpElement
+ *
+ * @package simplesamlphp/saml2
+ */
+final class GetCompleteTest extends TestCase
+{
+    use SchemaValidationTestTrait;
+    use SerializableElementTestTrait;
+
+    /**
+     */
+    public static function setUpBeforeClass(): void
+    {
+        self::$schemaFile = dirname(__FILE__, 5) . '/resources/schemas/saml-schema-protocol-2.0.xsd';
+
+        self::$testedClass = GetComplete::class;
+
+        self::$xmlRepresentation = DOMDocumentFactory::fromFile(
+            dirname(__FILE__, 4) . '/resources/xml/samlp_GetComplete.xml',
+        );
+    }
+
+
+    /**
+     */
+    public function testMarshalling(): void
+    {
+        $getComplete = new GetComplete('https://some/location');
+
+        $getCompleteElement = $getComplete->toXML();
+        $this->assertEquals('https://some/location', $getCompleteElement->textContent);
+
+        $this->assertEquals(
+            self::$xmlRepresentation->saveXML(self::$xmlRepresentation->documentElement),
+            strval($getComplete),
+        );
+    }
+
+
+    /**
+     */
+    public function testUnmarshalling(): void
+    {
+        $getComplete = GetComplete::fromXML(self::$xmlRepresentation->documentElement);
+
+        $this->assertEquals(
+            self::$xmlRepresentation->saveXML(self::$xmlRepresentation->documentElement),
+            strval($getComplete),
+        );
+    }
+}
