13c304baac59af1a9353893b1bd6b8d21b9a334c
Tim van Dijen <tvdijen@gmail.com>
2023-06-10 22:49:23 +0200

    Backport samlp:IDPList

---
+++ src/SAML2/XML/samlp/IDPList.php
@@ -0,0 +1,118 @@
+<?php
+
+declare(strict_types=1);
+
+namespace SimpleSAML\SAML2\XML\samlp;
+
+use DOMElement;
+use SimpleSAML\Assert\Assert;
+use SimpleSAML\XML\Constants as C;
+use SimpleSAML\XML\Exception\InvalidDOMElementException;
+use SimpleSAML\XML\Exception\MissingElementException;
+use SimpleSAML\XML\Exception\TooManyElementsException;
+use SimpleSAML\XML\Utils as XMLUtils;
+
+use function array_pop;
+use function is_null;
+
+/**
+ * Class for handling SAML2 IDPList.
+ *
+ * @package simplesamlphp/saml2
+ */
+final class IDPList extends AbstractSamlpElement
+{
+    /**
+     * Initialize an IDPList element.
+     *
+     * @param \SimpleSAML\SAML2\XML\samlp\IDPEntry[] $idpEntry
+     * @param \SimpleSAML\SAML2\XML\samlp\GetComplete|null $getComplete
+     */
+    public function __construct(
+        protected array $IDPEntry,
+        protected ?GetComplete $getComplete = null,
+    ) {
+        Assert::maxCount($IDPEntry, C::UNBOUNDED_LIMIT);
+        Assert::minCount($IDPEntry, 1, 'At least one samlp:IDPEntry must be specified.');
+        Assert::allIsInstanceOf($IDPEntry, IDPEntry::class);
+    }
+
+
+    /**
+     * @return \SimpleSAML\SAML2\XML\samlp\IDPEntry[]
+     */
+    public function getIdpEntry(): array
+    {
+        return $this->IDPEntry;
+    }
+
+
+    /**
+     * @return \SimpleSAML\SAML2\XML\samlp\GetComplete|null
+     */
+    public function getGetComplete(): ?GetComplete
+    {
+        return $this->getComplete;
+    }
+
+
+    /**
+     * Convert XML into a IDPList-element
+     *
+     * @param \DOMElement $xml The XML element we should load
+     * @return static
+     *
+     * @throws \SimpleSAML\XML\Exception\InvalidDOMElementException
+     *   if the qualified name of the supplied element is wrong
+     * @throws \SimpleSAML\XML\Exception\MissingElementException
+     *   if one of the mandatory child-elements is missing
+     * @throws \SimpleSAML\XML\Exception\TooManyElementsException
+     *   if too many child-elements of a type are specified
+     */
+    public static function fromXML(DOMElement $xml): static
+    {
+        Assert::same($xml->localName, 'IDPList', InvalidDOMElementException::class);
+        Assert::same($xml->namespaceURI, IDPList::NS, InvalidDOMElementException::class);
+
+        $idpEntry = IDPEntry::getChildrenOfClass($xml);
+        Assert::minCount(
+            $idpEntry,
+            1,
+            'At least one <samlp:IDPEntry> must be specified.',
+            MissingElementException::class,
+        );
+
+        $getComplete = GetComplete::getChildrenOfClass($xml);
+        Assert::maxCount(
+            $getComplete,
+            1,
+            'Only one <samlp:GetComplete> element is allowed.',
+            TooManyElementsException::class,
+        );
+
+        return new static(
+            $idpEntry,
+            empty($getComplete) ? null : array_pop($getComplete),
+        );
+    }
+
+
+    /**
+     * Convert this IDPList to XML.
+     *
+     * @param \DOMElement|null $parent The element we should append this IDPList to.
+     * @return \DOMElement
+     */
+    public function toXML(DOMElement $parent = null): DOMElement
+    {
+        $e = $this->instantiateParentElement($parent);
+
+        foreach ($this->getIDPEntry() as $idpEntry) {
+            $idpEntry->toXML($e);
+        }
+
+        $this->getGetComplete()?->toXML($e);
+
+        return $e;
+    }
+}
---
+++ tests/SAML2/XML/samlp/IDPListTest.php
@@ -0,0 +1,149 @@
+<?php
+
+declare(strict_types=1);
+
+namespace SimpleSAML\Test\SAML2\XML\samlp;
+
+use DOMDocument;
+use PHPUnit\Framework\TestCase;
+use SimpleSAML\SAML2\Utils\XPath;
+use SimpleSAML\SAML2\XML\samlp\GetComplete;
+use SimpleSAML\SAML2\XML\samlp\IDPEntry;
+use SimpleSAML\SAML2\XML\samlp\IDPList;
+use SimpleSAML\XML\DOMDocumentFactory;
+use SimpleSAML\XML\Exception\MissingElementException;
+use SimpleSAML\XML\TestUtils\SchemaValidationTestTrait;
+use SimpleSAML\XML\TestUtils\SerializableElementTestTrait;
+
+use function dirname;
+use function strval;
+
+/**
+ * Class \SAML2\XML\samlp\IDPListTest
+ *
+ * @covers \SimpleSAML\SAML2\XML\samlp\IDPList
+ * @covers \SimpleSAML\SAML2\XML\samlp\AbstractSamlpElement
+ *
+ * @package simplesamlphp/saml2
+ */
+final class IDPListTest extends TestCase
+{
+    use SchemaValidationTestTrait;
+    use SerializableElementTestTrait;
+
+
+    /**
+     */
+    public static function setUpBeforeClass(): void
+    {
+        self::$schemaFile = dirname(__FILE__, 5) . '/resources/schemas/saml-schema-protocol-2.0.xsd';
+
+        self::$testedClass = IDPList::class;
+
+        self::$xmlRepresentation = DOMDocumentFactory::fromFile(
+            dirname(__FILE__, 4) . '/resources/xml/samlp_IDPList.xml',
+        );
+    }
+
+
+    /**
+     */
+    public function testMarshalling(): void
+    {
+        $entry1 = new IDPEntry('urn:some:requester1', 'testName1', 'urn:test:testLoc1');
+        $entry2 = new IDPEntry('urn:some:requester2', 'testName2', 'urn:test:testLoc2');
+        $getComplete = new GetComplete('https://some/location');
+        $list = new IDPList([$entry1, $entry2], $getComplete);
+
+        $this->assertEquals(
+            self::$xmlRepresentation->saveXML(self::$xmlRepresentation->documentElement),
+            strval($list),
+        );
+    }
+
+
+    /**
+     */
+    public function testMarshallingElementOrdering(): void
+    {
+        $entry1 = new IDPEntry('urn:some:requester1', 'testName1', 'urn:test:testLoc1');
+        $entry2 = new IDPEntry('urn:some:requester2', 'testName2', 'urn:test:testLoc2');
+        $getComplete = new GetComplete('https://some/location');
+        $list = new IDPList([$entry1, $entry2], $getComplete);
+
+        $listElement = $list->toXML();
+
+        // Test for an IDPEntry
+        $xpCache = XPath::getXPath($listElement);
+        $listElements = XPath::xpQuery($listElement, './saml_protocol:IDPEntry', $xpCache);
+        $this->assertCount(2, $listElements);
+
+        // Test ordering of IDPList contents
+        /** @psalm-var \DOMElement[] $listElements */
+        $listElements = XPath::xpQuery($listElement, './saml_protocol:IDPEntry/following-sibling::*', $xpCache);
+        $this->assertCount(2, $listElements);
+        $this->assertEquals('samlp:IDPEntry', $listElements[0]->tagName);
+        $this->assertEquals('samlp:GetComplete', $listElements[1]->tagName);
+    }
+
+
+    /**
+     */
+    public function testMarshallingNullables(): void
+    {
+        $ns = IDPList::NS;
+        $document = <<<XML
+<samlp:IDPList xmlns:samlp="{$ns}">
+  <samlp:IDPEntry ProviderID="urn:some:requester1" Name="testName1" Loc="urn:test:testLoc1"/>
+</samlp:IDPList>
+XML
+        ;
+
+        $entry1 = new IDPEntry('urn:some:requester1', 'testName1', 'urn:test:testLoc1');
+        $list = new IDPList([$entry1]);
+
+        $entries = $list->getIdpEntry();
+        $this->assertCount(1, $entries);
+
+        $this->assertEquals('urn:some:requester1', $entries[0]->getProviderID());
+        $this->assertEquals('testName1', $entries[0]->getName());
+        $this->assertEquals('urn:test:testLoc1', $entries[0]->getLoc());
+
+        $this->assertNull($list->getGetComplete());
+
+        $this->assertEquals($document, strval($list));
+    }
+
+
+    /**
+     */
+    public function testUnmarshalling(): void
+    {
+        $list = IDPList::fromXML(self::$xmlRepresentation->documentElement);
+
+        $this->assertEquals(
+            self::$xmlRepresentation->saveXML(self::$xmlRepresentation->documentElement),
+            strval($list),
+        );
+    }
+
+
+    /**
+     */
+    public function testZeroIDPEntriesThrowsException(): void
+    {
+        $ns = IDPList::NS;
+
+        $xmlRepresentation = DOMDocumentFactory::fromString(<<<XML
+<samlp:IDPList xmlns:samlp="{$ns}">
+  <samlp:GetComplete>https://some/location</samlp:GetComplete>
+</samlp:IDPList>
+XML
+        );
+
+        $this->expectException(MissingElementException::class);
+        $this->expectExceptionMessage('At least one <samlp:IDPEntry> must be specified.');
+
+        IDPList::fromXML($xmlRepresentation->documentElement);
+    }
+}
