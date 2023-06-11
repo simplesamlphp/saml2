8264e557847c42ce8984be486374fcc1449a648e
Tim van Dijen <tvdijen@gmail.com>
2023-06-11 15:31:32 +0200

    Backport samlp:StatusCode

---
+++ tests/SAML2/XML/samlp/StatusCodeTest.php
@@ -0,0 +1,77 @@
+<?php
+
+declare(strict_types=1);
+
+namespace SimpleSAML\Test\SAML2\XML\samlp;
+
+use DOMDocument;
+use PHPUnit\Framework\TestCase;
+use SimpleSAML\SAML2\Constants as C;
+use SimpleSAML\SAML2\Utils;
+use SimpleSAML\SAML2\XML\samlp\StatusCode;
+use SimpleSAML\XML\DOMDocumentFactory;
+use SimpleSAML\XML\TestUtils\SchemaValidationTestTrait;
+use SimpleSAML\XML\TestUtils\SerializableElementTestTrait;
+
+use function dirname;
+use function strval;
+
+/**
+ * Class \SAML2\XML\samlp\StatusCodeTest
+ *
+ * @covers \SimpleSAML\SAML2\XML\samlp\StatusCode
+ * @covers \SimpleSAML\SAML2\XML\samlp\AbstractSamlpElement
+ *
+ * @package simplesamlphp/saml2
+ */
+final class StatusCodeTest extends TestCase
+{
+    use SchemaValidationTestTrait;
+    use SerializableElementTestTrait;
+
+
+    /**
+     */
+    public static function setUpBeforeClass(): void
+    {
+        self::$schemaFile = dirname(__FILE__, 5) . '/resources/schemas/saml-schema-protocol-2.0.xsd';
+
+        self::$testedClass = StatusCode::class;
+
+        self::$xmlRepresentation = DOMDocumentFactory::fromFile(
+            dirname(__FILE__, 4) . '/resources/xml/samlp_StatusCode.xml',
+        );
+    }
+
+
+    /**
+     */
+    public function testMarshalling(): void
+    {
+
+        $statusCode = new StatusCode(
+            C::STATUS_RESPONDER,
+            [
+                new StatusCode(C::STATUS_REQUEST_DENIED),
+            ],
+        );
+
+        $this->assertEquals(
+            self::$xmlRepresentation->saveXML(self::$xmlRepresentation->documentElement),
+            strval($statusCode),
+        );
+    }
+
+
+    /**
+     */
+    public function testUnmarshalling(): void
+    {
+        $statusCode = StatusCode::fromXML(self::$xmlRepresentation->documentElement);
+
+        $this->assertEquals(
+            self::$xmlRepresentation->saveXML(self::$xmlRepresentation->documentElement),
+            strval($statusCode),
+        );
+    }
+}
