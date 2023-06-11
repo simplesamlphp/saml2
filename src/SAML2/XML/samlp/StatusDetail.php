1cb6885618840cb4359ac63cd7b67891700f8012
Tim van Dijen <tvdijen@gmail.com>
2023-06-11 15:30:55 +0200

    Backport samlp:StatusDetail

---
+++ src/SAML2/XML/samlp/StatusDetail.php
@@ -0,0 +1,94 @@
+<?php
+
+declare(strict_types=1);
+
+namespace SimpleSAML\SAML2\XML\samlp;
+
+use DOMElement;
+use SimpleSAML\Assert\Assert;
+use SimpleSAML\XML\Exception\InvalidDOMElementException;
+use SimpleSAML\XML\Chunk;
+use SimpleSAML\XML\Constants as C;
+use SimpleSAML\XML\ExtendableElementTrait;
+
+/**
+ * SAML StatusDetail data type.
+ *
+ * @package simplesamlphp/saml2
+ */
+final class StatusDetail extends AbstractSamlpElement
+{
+    use ExtendableElementTrait;
+
+    /** The namespace-attribute for the xs:any element */
+    public const XS_ANY_ELT_NAMESPACE = C::XS_ANY_NS_ANY;
+
+
+    /**
+     * Initialize a samlp:StatusDetail
+     *
+     * @param \SimpleSAML\XML\Chunk[] $details
+     */
+    public function __construct(array $details = [])
+    {
+        $this->setElements($details);
+    }
+
+
+    /**
+     * Test if an object, at the state it's in, would produce an empty XML-element
+     *
+     * @return bool
+     */
+    public function isEmptyElement(): bool
+    {
+        return empty($this->elements);
+    }
+
+
+    /**
+     * Convert XML into a StatusDetail
+     *
+     * @param \DOMElement $xml The XML element we should load
+     * @return static
+     *
+     * @throws \SimpleSAML\XML\Exception\InvalidDOMElementException
+     *   if the qualified name of the supplied element is wrong
+     */
+    public static function fromXML(DOMElement $xml): static
+    {
+        Assert::same($xml->localName, 'StatusDetail', InvalidDOMElementException::class);
+        Assert::same($xml->namespaceURI, StatusDetail::NS, InvalidDOMElementException::class);
+
+        $details = [];
+        foreach ($xml->childNodes as $detail) {
+            if (!($detail instanceof DOMElement)) {
+                continue;
+            }
+
+            $details[] = new Chunk($detail);
+        }
+
+        return new static($details);
+    }
+
+
+    /**
+     * Convert this StatusDetail to XML.
+     *
+     * @param \DOMElement|null $element The element we are converting to XML.
+     * @return \DOMElement The XML element after adding the data corresponding to this StatusDetail.
+     */
+    public function toXML(DOMElement $parent = null): DOMElement
+    {
+        /** @psalm-var \DOMDocument $e->ownerDocument */
+        $e = $this->instantiateParentElement($parent);
+
+        foreach ($this->getElements() as $detail) {
+            /** @psalm-var \SimpleSAML\XML\SerializableElementInterface $detail */
+            $detail->toXML($e);
+        }
+
+        return $e;
+    }
+}
---
+++ tests/SAML2/XML/samlp/StatusDetailTest.php
@@ -0,0 +1,90 @@
+<?php
+
+declare(strict_types=1);
+
+namespace SimpleSAML\Test\SAML2\XML\samlp;
+
+use DOMDocument;
+use PHPUnit\Framework\TestCase;
+use SimpleSAML\SAML2\Constants as C;
+use SimpleSAML\SAML2\XML\samlp\StatusDetail;
+use SimpleSAML\XML\Chunk;
+use SimpleSAML\XML\DOMDocumentFactory;
+use SimpleSAML\XML\TestUtils\SchemaValidationTestTrait;
+use SimpleSAML\XML\TestUtils\SerializableElementTestTrait;
+
+use function dirname;
+use function strval;
+
+/**
+ * Class \SAML2\XML\samlp\StatusDetailTest
+ *
+ * @covers \SimpleSAML\SAML2\XML\samlp\StatusDetail
+ * @covers \SimpleSAML\SAML2\XML\samlp\AbstractSamlpElement
+ *
+ * @package simplesamlphp/saml2
+ */
+final class StatusDetailTest extends TestCase
+{
+    use SchemaValidationTestTrait;
+    use SerializableElementTestTrait;
+
+
+    /**
+     */
+    public static function setUpBeforeClass(): void
+    {
+        self::$schemaFile = dirname(__FILE__, 5) . '/resources/schemas/saml-schema-protocol-2.0.xsd';
+
+        self::$testedClass = StatusDetail::class;
+
+        self::$xmlRepresentation = DOMDocumentFactory::fromFile(
+            dirname(__FILE__, 4) . '/resources/xml/samlp_StatusDetail.xml',
+        );
+    }
+
+
+    /**
+     */
+    public function testMarshalling(): void
+    {
+        $document = DOMDocumentFactory::fromString(
+            '<ssp:Cause xmlns:ssp="urn:custom:ssp">org.sourceid.websso.profiles.idp.FailedAuthnSsoException</ssp:Cause>',
+        );
+
+        $statusDetail = new StatusDetail([new Chunk($document->documentElement)]);
+
+        $this->assertEquals(
+            self::$xmlRepresentation->saveXML(self::$xmlRepresentation->documentElement),
+            strval($statusDetail),
+        );
+    }
+
+
+    /**
+     * Adding an empty StatusDetail element should yield an empty element.
+     */
+    public function testMarshallingEmptyElement(): void
+    {
+        $samlpns = C::NS_SAMLP;
+        $statusDetail = new StatusDetail([]);
+        $this->assertEquals(
+            "<samlp:StatusDetail xmlns:samlp=\"$samlpns\"/>",
+            strval($statusDetail),
+        );
+        $this->assertTrue($statusDetail->isEmptyElement());
+    }
+
+
+    /**
+     */
+    public function testUnmarshalling(): void
+    {
+        $statusDetail = StatusDetail::fromXML(self::$xmlRepresentation->documentElement);
+
+        $this->assertEquals(
+            self::$xmlRepresentation->saveXML(self::$xmlRepresentation->documentElement),
+            strval($statusDetail),
+        );
+    }
+}
