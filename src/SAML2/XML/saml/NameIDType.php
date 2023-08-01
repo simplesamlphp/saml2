20c50b4e6d357c903530cec7ca02a50e1377a759
Tim van Dijen <tvdijen@gmail.com>
2023-08-02 00:02:26 +0200

    Make NameID arrayizable

--- src/SAML2/XML/saml/NameIDType.php
+++ src/SAML2/XML/saml/NameIDType.php
@@ -7,6 +7,7 @@ namespace SimpleSAML\SAML2\XML\saml;
 use DOMElement;
 use SimpleSAML\Assert\Assert;
 use SimpleSAML\SAML2\Compat\ContainerSingleton;
+use SimpleSAML\XML\Exception\ArrayValidationException;
 use SimpleSAML\XML\StringElementTrait;
 use SimpleSAML\XMLSecurity\Backend\EncryptionBackend;
 
@@ -101,5 +102,96 @@ abstract class NameIDType extends AbstractBaseIDType
 
         $e->textContent = $this->getContent();
         return $e;
+    }
+
+
+    /**
+     * Create a class from an array
+     *
+     * @param array $data
+     * @return static
+     */
+    public static function fromArray(array $data): static
+    {
+        $data = self::processArrayContents($data);
+
+        return new static(
+            $data['value'],
+            $data['NameQualifier'] ?? null,
+            $data['SPNameQualifier'] ?? null,
+            $data['Format'] ?? null,
+            $data['SPProvidedID'] ?? null,
+        );
+    }
+
+
+    /**
+     * Validates an array representation of this object and returns the same array with
+     * rationalized keys (casing) and parsed sub-elements.
+     *
+     * @param array $data
+     * @return array $data
+     */
+    private static function processArrayContents(array $data): array
+    {
+        $data = array_change_key_case($data, CASE_LOWER);
+
+        // Make sure the array keys are known for this kind of object
+        Assert::allOneOf(
+            array_keys($data),
+            [
+                'value',
+                'format',
+                'namequalifier',
+                'spnamequalifier',
+                'spprovidedid',
+            ],
+            ArrayValidationException::class,
+        );
+
+        Assert::keyExists($data, 'value', ArrayValidationException::class);
+        Assert::string($data['value'], ArrayValidationException::class);
+        $retval = ['value' => $data['value']];
+
+        if (array_key_exists('format', $data)) {
+            Assert::string($data['format'], ArrayValidationException::class);
+            $retval['Format'] = $data['format'];
+        }
+
+        if (array_key_exists('namequalifier', $data)) {
+            Assert::string($data['namequalifier'], ArrayValidationException::class);
+            $retval['NameQualifier'] = $data['namequalifier'];
+        }
+
+        if (array_key_exists('spnamequalifier', $data)) {
+            Assert::string($data['spnamequalifier'], ArrayValidationException::class);
+            $retval['SPNameQualifier'] = $data['spnamequalifier'];
+        }
+
+        if (array_key_exists('spprovidedid', $data)) {
+            Assert::string($data['spprovidedid'], ArrayValidationException::class);
+            $retval['SPProvidedID'] = $data['spprovidedid'];
+        }
+
+        return $retval;
+    }
+
+
+    /**
+     * Create an array from this class
+     *
+     * @return array
+     */
+    public function toArray(): array
+    {
+        $data = [
+            'value' => $this->getContent(),
+            'Format' => $this->getFormat(),
+            'NameQualifier' => $this->getNameQualifier(),
+            'SPNameQualifier' => $this->getSPNameQualifier(),
+            'SPProvidedID' => $this->getSPProvidedID(),
+        ];
+
+        return array_filter($data);
     }
 }
