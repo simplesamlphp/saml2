c92125c8300183af17721a2ddec5391a51fc4446
Tim van Dijen <tvdijen@gmail.com>
2023-08-02 20:40:28 +0200

    Make NameID arrayizable

--- tests/SAML2/XML/saml/NameIDTest.php
+++ tests/SAML2/XML/saml/NameIDTest.php
@@ -8,6 +8,7 @@ use DOMDocument;
 use PHPUnit\Framework\TestCase;
 use SimpleSAML\SAML2\XML\saml\NameID;
 use SimpleSAML\XML\DOMDocumentFactory;
+use SimpleSAML\XML\TestUtils\ArrayizableElementTestTrait;
 use SimpleSAML\XML\TestUtils\SchemaValidationTestTrait;
 use SimpleSAML\XML\TestUtils\SerializableElementTestTrait;
 
@@ -25,6 +26,7 @@ use function strval;
  */
 final class NameIDTest extends TestCase
 {
+    use ArrayizableElementTestTrait;
     use SchemaValidationTestTrait;
     use SerializableElementTestTrait;
 
@@ -36,6 +38,14 @@ final class NameIDTest extends TestCase
         self::$schemaFile = dirname(__FILE__, 5) . '/resources/schemas/saml-schema-assertion-2.0.xsd';
 
         self::$testedClass = NameID::class;
+
+        self::$arrayRepresentation = [
+            'value' => 'TheNameIDValue',
+            'Format' => 'urn:the:format',
+            'NameQualifier' => 'TheNameQualifier',
+            'SPNameQualifier' => 'TheSPNameQualifier',
+            'SPProvidedID' => 'TheSPProvidedID',
+        ];
 
         self::$xmlRepresentation = DOMDocumentFactory::fromFile(
             dirname(__FILE__, 4) . '/resources/xml/saml_NameID.xml',
