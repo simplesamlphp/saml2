23b3e1da8da17aceb91c3f5eb45ceb02c078218a
Tim van Dijen <tvdijen@gmail.com>
2024-04-16 19:44:41 +0200

    Test arrayizability of samlp:RequesterID

--- tests/SAML2/XML/samlp/RequesterIDTest.php
+++ tests/SAML2/XML/samlp/RequesterIDTest.php
@@ -10,6 +10,7 @@ use PHPUnit\Framework\TestCase;
 use SimpleSAML\SAML2\XML\samlp\AbstractSamlpElement;
 use SimpleSAML\SAML2\XML\samlp\RequesterID;
 use SimpleSAML\XML\DOMDocumentFactory;
+use SimpleSAML\XML\TestUtils\ArrayizableElementTestTrait;
 use SimpleSAML\XML\TestUtils\SchemaValidationTestTrait;
 use SimpleSAML\XML\TestUtils\SerializableElementTestTrait;
 
@@ -26,6 +27,7 @@ use function strval;
 #[CoversClass(AbstractSamlpElement::class)]
 final class RequesterIDTest extends TestCase
 {
+    use ArrayizableElementTestTrait;
     use SchemaValidationTestTrait;
     use SerializableElementTestTrait;
 
@@ -40,6 +42,8 @@ final class RequesterIDTest extends TestCase
         self::$xmlRepresentation = DOMDocumentFactory::fromFile(
             dirname(__FILE__, 4) . '/resources/xml/samlp_RequesterID.xml',
         );
+
+        self::$arrayRepresentation = ['urn:some:requester'];
     }
 
 
