caf8436faa685f4f05117d636e10abc5e9e881be
Tim van Dijen <tvdijen@gmail.com>
2024-07-23 12:00:52 +0200

    Fix quality checks

--- src/SAML2/XML/md/NameIDFormat.php
+++ src/SAML2/XML/md/NameIDFormat.php
@@ -10,6 +10,8 @@ use SimpleSAML\XML\Exception\InvalidDOMElementException;
 use SimpleSAML\XML\Exception\SchemaViolationException;
 use SimpleSAML\XML\StringElementTrait;
 
+use function trim;
+
 /**
  * Class representing a md:NameIDFormat element.
  *
@@ -26,6 +28,42 @@ final class NameIDFormat extends AbstractMdElement
     public function __construct(string $content)
     {
         $this->setContent($content);
+    }
+
+
+    /**
+     * Get the content of the element.
+     *
+     * @return string
+     */
+    public function getContent(): string
+    {
+        return $this->sanitizeContent($this->getRawContent());
+    }
+
+
+    /**
+     * Get the raw and unsanitized content of the element.
+     *
+     * @return string
+     */
+    public function getRawContent(): string
+    {
+        return $this->content;
+    }
+
+
+    /**
+     * Sanitize the content of the element.
+     *
+     * @param string $content  The unsanitized textContent
+     * @throws \Exception on failure
+     * @return string
+     */
+    protected function sanitizeContent(string $content): string
+    {
+        // We've seen metadata in the wild that had stray whitespace around URIs, causing assertions to fail
+        return trim($content);
     }
 
 
