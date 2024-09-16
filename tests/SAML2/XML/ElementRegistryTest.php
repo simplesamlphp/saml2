4178b63d4c2e64e42e786fb32308701c02fe0bd7
Tim van Dijen <tvdijen@gmail.com>
2024-09-17 00:18:42 +0200

    Add element registry

---
+++ tests/SAML2/XML/ElementRegistryTest.php
@@ -0,0 +1,37 @@
+<?php
+
+declare(strict_types=1);
+
+namespace SimpleSAML\Test\SAML2\XML;
+
+use PHPUnit\Framework\Attributes\Group;
+use PHPUnit\Framework\TestCase;
+
+use function dirname;
+use function sprintf;
+
+/**
+ * Tests for element registry.
+ *
+ * @package simplesamlphp/saml2
+ */
+#[Group('utils')]
+final class ElementRegistryTest extends TestCase
+{
+    /**
+     * Test that the class-name can be resolved and it's localname matches.
+     */
+    public function testElementRegistry(): void
+    {
+        $elementRegistry = dirname(__FILE__, 4) . '/src/XML/element.registry.php';
+        $namespaces = include($elementRegistry);
+
+        foreach ($namespaces as $namespaceURI => $elements) {
+            foreach ($elements as $localName => $fqdn) {
+                $this->assertTrue(class_exists($fqdn), sprintf('Class \'%s\' could not be found.', $fqdn));
+                $this->assertEquals($fqdn::getLocalName(), $localName);
+                $this->assertEquals($fqdn::getNamespaceURI(), $namespaceURI);
+            }
+        }
+    }
+}
