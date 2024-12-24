92085dbbd6d87f87dcfafcd25efb958fc710364c
Tim van Dijen <tvdijen@gmail.com>
2024-12-24 14:21:00 +0100

    Fix XPath constructor

--- tests/SAML2/XML/saml/EncryptedAssertionTest.php
+++ tests/SAML2/XML/saml/EncryptedAssertionTest.php
@@ -92,7 +92,7 @@ final class EncryptedAssertionTest extends TestCase
     public function testMarshalling(): void
     {
         $transforms = new Transforms(
-            [new Transform(C::XPATH10_URI, new XPath('self::xenc:CipherValue[@Id="example1"]', ['xenc' => C::NS_XENC]))],
+            [new Transform(C::XPATH10_URI, new XPath('self::xenc:CipherValue[@Id="example1"]'))],
         );
 
         $retrievalMethod = new RetrievalMethod($transforms, '#Encrypted_KEY_ID', C::XMLENC_ENCRYPTEDKEY);
