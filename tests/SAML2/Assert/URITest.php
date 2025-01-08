8735c79aaaaeef0b952a656325457cc1f123936d
Tim van Dijen <tvdijen@gmail.com>
2025-01-08 18:39:57 +0100

    Remove incorrect test; diacritical characters are allowed in URI

--- tests/SAML2/Assert/URITest.php
+++ tests/SAML2/Assert/URITest.php
@@ -44,7 +44,7 @@ final class URITest extends TestCase
             'urn' => [true, 'urn:x-simplesamlphp:phpunit'],
             'same-doc' => [false, '#_53d830ab1be17291a546c95c7f1cdf8d3d23c959e6'],
             'url' => [true, 'https://www.simplesamlphp.org'],
-            'invalid_char' => [false, 'https://a⒈com'],
+            'utf8_char' => [true, 'https://aä.com'],
             'intl' => [true, 'https://niño.com'],
             'spn' => [true, 'spn:a4cf592f-a64c-46ff-a788-b260f474525b'],
             'typos' => [false, 'https//www.uni.l/en/'],
