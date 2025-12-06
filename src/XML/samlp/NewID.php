f93cb662dd1b7c8fa7b52ab4a2dc2ebf2069f0df
Tim van Dijen <tvdijen@gmail.com>
2025-12-06 17:57:50 +0100

    Add missing samlp:NewID

---
+++ src/XML/samlp/NewID.php
@@ -0,0 +1,25 @@
+<?php
+
+declare(strict_types=1);
+
+namespace SimpleSAML\SAML2\XML\samlp;
+
+use SimpleSAML\SAML2\Type\SAMLStringValue;
+use SimpleSAML\XML\SchemaValidatableElementInterface;
+use SimpleSAML\XML\SchemaValidatableElementTrait;
+use SimpleSAML\XML\TypedTextContentTrait;
+
+/**
+ * Class representing a samlp:NewID element.
+ *
+ * @package simplesaml/saml2
+ */
+final class NewID extends AbstractSamlpElement implements SchemaValidatableElementInterface
+{
+    use SchemaValidatableElementTrait;
+    use TypedTextContentTrait;
+
+
+    /** @var string */
+    public const TEXTCONTENT_TYPE = SAMLStringValue::class;
+}
