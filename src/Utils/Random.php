7a158e387a3d74f845cf926962e18611c3464492
Tim van Dijen <tvdijen@gmail.com>
2025-12-17 17:16:01 +0100

    Add local SAML2-compliant version of the Random ID-generator

---
+++ src/Utils/Random.php
@@ -0,0 +1,23 @@
+<?php
+
+declare(strict_types=1);
+
+namespace SimpleSAML\XML\Utils;
+
+use function bin2hex;
+use function random_bytes;
+
+/**
+ * @package simplesamlphp/saml2
+ */
+class Random extends \SimpleSAML\XML\Utils\Random
+{
+    /**
+     * The fixed length of random identifiers.
+     *
+     * This results in the maximum of 160 bits entropy specified in paragraph 1.3.4 of the SAML 2.0 core specifications
+     *
+     * (41 - 1) / 2 = 20 → random_bytes(20) → 160 bits
+     */
+    public const int ID_LENGTH = 41;
+}
